c9e5707ebd29b9e87f823406032582d426d40f7d
Tobias <tobias@pop-os.localdomain>
2020-10-14 14:19:56 -0700

    added config file

---
+++ parser.py
@@ -0,0 +1,19 @@
+#
+# Tobias Rubel | rubelato@reed.edu
+# Reed CompBio
+#
+# This script just parses the yaml config files
+
+
+import yaml
+import os
+import sys
+
+def main(argv):
+    f = argv[1]
+    with open(f,'r') as g:
+        print(yaml.safe_load(g))
+
+
+if __name__ == "__main__":
+    main(sys.argv)
