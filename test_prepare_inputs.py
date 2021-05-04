271044ff8224b63371cb617250d0c50fd59dc0a5
Anthony Gitter <gitter@biostat.wisc.edu>
2021-05-04 16:28:47 -0500

    Fix test_prepare_inputs errors

--- test_prepare_inputs.py
+++ test_prepare_inputs.py
@@ -1,4 +1,5 @@
 import PRRunner
+import shutil
 import yaml
 import os
 
@@ -7,8 +8,11 @@ config_loc = os.path.join("config","config.yaml")
 with open(config_loc) as config_file:
     config = yaml.load(config_file, Loader=yaml.FullLoader)
 test_file = "test_pickled_dataset.pkl"
-PRRunner.merge_input(config, 0, test_file)
+PRRunner.merge_input(config["datasets"][0], test_file)
 print(PRRunner.get_required_inputs("pathlinker"))
-os.makedirs("output", exist_ok=True) #it is assumed directories will be made upstream
-PRRunner.prepare_inputs("output/","pathlinker",test_file,{})
+os.makedirs("tmp_output", exist_ok=True) #it is assumed directories will be made upstream
+filename_map = {"nodetypes": os.path.join("tmp_output", "nodetypes.txt"),
+                "network": os.path.join("tmp_output", "network.txt")}
+PRRunner.prepare_inputs("pathlinker",test_file,filename_map)
 os.remove(test_file)
+shutil.rmtree("tmp_output")
