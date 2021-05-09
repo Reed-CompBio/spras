a5dd64e13b84a06b95768bb80117e4b5e5686d99
Anna Ritz <aritz@reed.edu>
2021-05-09 09:16:58 -0700

    rewrote the save() function to be human-readable rather than JSON output. We want this to be a usable outfile for people who may not be familiar with JSON.

--- src/analysis/summary/summary.py
+++ src/analysis/summary/summary.py
@@ -1,6 +1,6 @@
 import sys
 import networkx as nx
-import json
+#import json
 #wrapper functions for nx methods here
 
 def degree(G):
@@ -30,9 +30,17 @@ def load_graph(path: str,directed=False) -> nx.Graph:
     #    print('file format not yet supported. submit a feature request if it ought to be!')
     return G
 
-def save(data,pth):
+def save_json(data,pth):
     with open(pth,'w') as f:
         json.dump(data,f)
+
+def save(data,pth):
+    fout = open (pth,'w')
+    fout.write('#node\t%s\n' % '\t'.join([s.__name__ for s in stats]))
+    for node in data[stats[0].__name__]:
+        row = [data[s.__name__][node] for s in stats]
+        fout.write('%s\t%s\n'% (node,'\t'.join([str(d) for d in row])))
+    fout.close()
 
 '''
 run function that wraps above functions.
