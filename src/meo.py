c6318e6d8068a5b2298688053ce906e7a13ee736
Anthony Gitter <gitter@biostat.wisc.edu>
2021-05-18 10:44:24 -0500

    Modify MEO entrypoint for chown call

--- src/meo.py
+++ src/meo.py
@@ -103,11 +103,14 @@ class MEO(PRM):
                 # This command changes the ownership of output files so we don't
                 # get a permissions error when snakemake tries to touch the files
                 chown_command = " ".join(["chown", str(uid), output_file, path_output_file.as_posix()])
+                # Modify the entrypoint because the command is expected to be a properties file that is passed
+                # to the jar file
                 client.containers.run('reedcompbio/meo',
                                       chown_command,
                                       stderr=True,
                                       volumes={prepare_path_docker(work_dir): {'bind': '/spras', 'mode': 'rw'}},
-                                      working_dir='/spras')
+                                      working_dir='/spras',
+                                      entrypoint='/bin/bash')
 
             print(out.decode('utf-8'))
         finally:
