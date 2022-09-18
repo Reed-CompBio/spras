5b4a2c990aa914b98e7bf515dbc7f01784049200
Neha Talluri <nehatalluri@Nehas-Air.lan>
2022-09-17 20:17:24 -0500

    added some of the code for generate inputs

---
+++ src/mincostflow.py
@@ -0,0 +1,30 @@
+from src.PRM import PRM
+from pathlib import Path
+from src.util import prepare_volume, run_container
+import pandas as pd
+
+__all__ = ['MinCostFlow']
+
+class MinCostFlow (PRM):
+    required_inputs = ['sources','targets','edges']
+
+    @staticmethod
+    def generate_inputs(data, filename_map):
+        
+        for input_type in MinCostFlow.required_inputs:
+            if input_type not in filename_map:
+                raise ValueError(f"{input_type} filename is missing")
+
+        for node_type in ['sources', 'targets']:
+            nodes = data.request_node_columns([node_type])
+            if nodes is None:
+                raise ValueError(f'No {node_type} found in the node files')
+            
+            nodes = nodes.loc[nodes[node_type]]
+            nodes.to_csv(filename_map[node_type], index=False, columns=['NODEID'], header=False)
+
+        edges = data.get_interactome()
+        # edges.insert(1, 'EdgeType', '(pp)') what does this do? do I need this?
+        edges.to_csv(filename_map['edges'], sep='\t', index=False, columns=['Interactor1', 'Interactor2', 'Weight'], header=False)
+
+
