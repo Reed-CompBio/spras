9e4d1eebe8c8d99fc76c789671bb754c1c75dade
Neha Talluri <nehatalluri@Nehas-MacBook-Air.local>
2022-09-21 15:50:34 -0500

    added comments

--- src/mincostflow.py
+++ src/mincostflow.py
@@ -11,20 +11,28 @@ class MinCostFlow (PRM):
     @staticmethod
     def generate_inputs(data, filename_map):
         
+        # ensures the required input are within the filename_map 
         for input_type in MinCostFlow.required_inputs:
             if input_type not in filename_map:
                 raise ValueError(f"{input_type} filename is missing")
 
+        # will take the sources and write them to files, and repeats with targets 
         for node_type in ['sources', 'targets']:
             nodes = data.request_node_columns([node_type])
             if nodes is None:
                 raise ValueError(f'No {node_type} found in the node files')
             
             nodes = nodes.loc[nodes[node_type]]
+
+            # creates with the node type without headers 
             nodes.to_csv(filename_map[node_type], index=False, columns=['NODEID'], header=False)
 
+        # create the network of edges 
         edges = data.get_interactome()
+        
         # edges.insert(1, 'EdgeType', '(pp)') what does this do? do I need this?
-        edges.to_csv(filename_map['edges'], sep='\t', index=False, columns=['Interactor1', 'Interactor2', 'Weight'], header=False)
+        
+        # creates the edges files that contains the head and tail nodes and the weights between them
+        edges.to_csv(filename_map['edges'], sep='\t', index=False, columns=['Node1', 'Node2', 'Weight'], header=False)
 
 
