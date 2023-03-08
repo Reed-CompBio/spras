70cf4f0e54ed00520d72dd9f88d398291faba4e5
ntalluri <nehatalluri@live.com>
2023-03-08 15:22:02 -0600

    trying to fix the tests

--- test/analysis/test_ml.py
+++ test/analysis/test_ml.py
@@ -16,13 +16,13 @@ class TestML:
 
     def test_pca(self):
         dataframe = ml.summarize_networks([TEST_DIR+'s1.txt',TEST_DIR+'s2.txt',TEST_DIR+'s3.txt'])
-        ml.pca(dataframe, OUT_DIR+'/pca/pca.png', OUT_DIR+'/pca/pca-components.txt', OUT_DIR+ '/pca/pca-coordinates.txt')
-        assert(filecmp.cmp(OUT_DIR+ '/pca/pca-coordinates.txt', EXPECT_DIR+ '/expected_coords.txt'))
-        assert(filecmp.cmp(OUT_DIR+'/pca/pca-components.txt', EXPECT_DIR+'/expected_components.txt'))
+        ml.pca(dataframe, OUT_DIR+'/pca.png', OUT_DIR+'/pca-components.txt', OUT_DIR+ '/pca-coordinates.txt')
+        assert(filecmp.cmp(OUT_DIR+ '/pca-coordinates.txt', EXPECT_DIR+ '/expected_coords.txt'))
+        assert(filecmp.cmp(OUT_DIR+'/pca-components.txt', EXPECT_DIR+'/expected_components.txt'))
     
     def test_hac(self):
         dataframe = ml.summarize_networks([TEST_DIR+'s1.txt',TEST_DIR+'s2.txt',TEST_DIR+'s3.txt'])
-        ml.hac(dataframe,OUT_DIR+'/hac/hac.png', OUT_DIR+'/hac/hac-clusters.txt' )
-        assert(filecmp.cmp(OUT_DIR+'/hac/hac-clusters.txt', EXPECT_DIR+'/expected_clusters.txt'))
+        ml.hac(dataframe,OUT_DIR+'/hac.png', OUT_DIR+'/hac-clusters.txt' )
+        assert(filecmp.cmp(OUT_DIR+'/hac-clusters.txt', EXPECT_DIR+'/expected_clusters.txt'))
 
         
