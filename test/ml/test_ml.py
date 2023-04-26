984a4b2736983a41c1fcd523bc789e227adbdc30
ntalluri <nehatalluri@live.com>
2023-04-26 13:46:00 -0500

    fixed ml tests

--- test/ml/test_ml.py
+++ test/ml/test_ml.py
@@ -17,15 +17,17 @@ class TestML:
         """
         Path(OUT_DIR).mkdir(parents=True, exist_ok=True)
 
+        
+
     def test_summarize_networks(self):
-        dataframe = ml.summarize_networks([INPUT_DIR + 's1/s1.txt', INPUT_DIR + 's2/s2.txt', INPUT_DIR + 's3/s3.txt',
-                                           INPUT_DIR + 'longName/longName.txt', INPUT_DIR + 'longName2/longName2.txt',
-                                           INPUT_DIR + 'empty/empty.txt'])
+        dataframe = ml.summarize_networks([INPUT_DIR + 'test-s1/s1.txt', INPUT_DIR + 'test-s2/s2.txt', INPUT_DIR + 'test-s3/s3.txt',
+                                           INPUT_DIR + 'test-longName/longName.txt', INPUT_DIR + 'test-longName2/test-longName2.txt',
+                                           INPUT_DIR + 'test-empty/empty.txt'])
         dataframe.to_csv(OUT_DIR + 'dataframe.csv')
         assert filecmp.cmp(OUT_DIR + 'dataframe.csv', EXPECT_DIR + 'expected_df.csv')
 
     def test_pca(self):
-        dataframe = ml.summarize_networks([INPUT_DIR + 's1/s1.txt', INPUT_DIR + 's2/s2.txt', INPUT_DIR + 's3/s3.txt'])
+        dataframe = ml.summarize_networks([INPUT_DIR + 'test-s1/s1.txt', INPUT_DIR + 'test-s2/s2.txt', INPUT_DIR + 'test-s3/s3.txt'])
         ml.pca(dataframe, OUT_DIR + 'pca/pca.png', OUT_DIR + 'pca/pca-components.txt',
                OUT_DIR + 'pca/pca-coordinates.csv')
         coord = pd.read_table(OUT_DIR + 'pca/pca-coordinates.csv')
@@ -36,7 +38,7 @@ class TestML:
         assert coord.equals(expected)
 
     def test_hac(self):
-        dataframe = ml.summarize_networks([INPUT_DIR + 's1/s1.txt', INPUT_DIR + 's2/s2.txt', INPUT_DIR + 's3/s3.txt'])
+        dataframe = ml.summarize_networks([INPUT_DIR + 'test-s1/s1.txt', INPUT_DIR + 'test-s2/s2.txt', INPUT_DIR + 'test-s3/s3.txt'])
         ml.hac(dataframe, OUT_DIR + 'hac/hac.png', OUT_DIR + 'hac/hac-clusters.txt')
 
         assert filecmp.cmp(OUT_DIR + 'hac/hac-clusters.txt', EXPECT_DIR + 'expected_clusters.txt')
