5755c62f558f6447fc7cd3c79a944a35a6a2cc0e
Erik_L <liue@reed.edu>
2023-06-05 22:44:08 -0700

    Make RWWR working (change Dockerfile for current random-walk-with-restart repo; with the modified output file, implemented the parsing function; met one error about Snakemake checking pathway.txt; need to write the tests after

---
+++ test/RandomWalk/test_rwwr.py
@@ -0,0 +1,19 @@
+import pytest
+import shutil
+from pathlib import Path
+from src.util import compare_files
+from src.random_walk import RandomWalk
+
+TEST_DIR = 'test/RandomWalk/'
+'''
+Need to think of some test files to use for this test.
+'''
+
+
+class TestRandomWalkWithRestart:
+    """
+    Run the local neighborhood algorithm on the example input files and check the output matches the expected output
+    """    
+    pass
+
+    # Write tests for the Local Neighborhood run function here
