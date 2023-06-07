d51fc3b64a698dc8874441c44a8784c03519b3ee
Erik_L <liue@reed.edu>
2023-06-06 19:00:25 -0700

    Make the program running (writing the tests); clean up the codes; update the README; more things to consider: how to select edges for pathway file.

--- src/random_walk.py
+++ src/random_walk.py
@@ -7,6 +7,7 @@ from src.util import prepare_volume, run_container
 __all__ = ['RandomWalk']
 
 class RandomWalk(PRM):
+    # we need edges (weighted), source set (with prizes), and target set (with prizes).
     required_inputs = ['edges', 'sources', 'targets']
 
     @staticmethod
@@ -25,8 +26,8 @@ class RandomWalk(PRM):
         # will take the sources and write them to files, and repeats with targets 
         for node_type in ['sources', 'targets']:                
             nodes = data.request_node_columns([node_type])  
+            # check if the nodes have prizes or not
             if data.contains_node_columns('prize'):
-                #NODEID is always included in the node table
                 node_df = data.request_node_columns(['prize'])
                 nodes = pd.merge(nodes, node_df, on='NODEID')
                 # creates with the node type without headers 
@@ -75,21 +76,15 @@ class RandomWalk(PRM):
         bind_path, targets_file = prepare_volume(targets, work_dir)
         volumes.append(bind_path)
 
-        print("REACHED HERE")
 
-        # PathLinker does not provide an argument to set the output directory
-        # Use its --output argument to set the output file prefix to specify an absolute path and prefix
         out_dir = Path(output_file).parent
-        # PathLinker requires that the output directory exist
+        # RandomWalk requires that the output directory exist
         out_dir.mkdir(parents=True, exist_ok=True)
         bind_path, mapped_out_dir = prepare_volume(str(out_dir), work_dir)
         volumes.append(bind_path)
         mapped_out_prefix= mapped_out_dir + '/out'  # Use posix path inside the container
         
 
-        # need 2 output files, one for nodes and one for edges (not implemented yet)
-        # if inputs are changed to output_nodes and output_edges, then the error will be "unexpected argument : output_file"
-        # if inputs are changed to only output_file, then the final output file will contain edges.
         command = ['python',
                    '/RandomWalk/random_walk.py',
                    '--edges_file', edges_file,
@@ -99,9 +94,8 @@ class RandomWalk(PRM):
                    '--selection_function', f,
                    '--output_file', mapped_out_prefix]
 
-        # print('Running RandomWalk with arguments: {}'.format(' '.join(str(command))), flush=True)
+        print('Running RandomWalk with arguments: {}'.format(' '.join(command)), flush=True)
 
-        print("HERE")
         # TODO consider making this a string in the config file instead of a Boolean
         container_framework = 'singularity' if singularity else 'docker'
         out = run_container(container_framework,
@@ -131,9 +125,8 @@ class RandomWalk(PRM):
         # get all rows where placeholder is Nan
         df_edge = df.loc[df["Placeholder"].isnull()]
 
-        standardized_pathway_file = standardized_pathway_file.replace('.txt', '')
-        edge_output_file = standardized_pathway_file + '_edges.txt'
-        node_output_file = standardized_pathway_file + '_nodes.txt'
+        edge_output_file = standardized_pathway_file
+        node_output_file = standardized_pathway_file.replace('.txt', '') + '_nodes.txt'
 
         # get rid of the placeholder column and output it to a file
         df_edge = df_edge.drop(columns=['Placeholder'])
--- test/RandomWalk/test_rwwr.py
+++ test/RandomWalk/test_rwwr.py
@@ -5,6 +5,10 @@ from src.util import compare_files
 from src.random_walk import RandomWalk
 
 TEST_DIR = 'test/RandomWalk/'
+OUT_FILES_1 = TEST_DIR + 'output1/rwr_pathway.txt'
+OUT_FILES_2 = TEST_DIR + 'output2/rwr_pathway.txt'
+
+
 '''
 Need to think of some test files to use for this test.
 '''
@@ -14,6 +18,76 @@ class TestRandomWalkWithRestart:
     """
     Run the local neighborhood algorithm on the example input files and check the output matches the expected output
     """    
-    pass
 
     # Write tests for the Local Neighborhood run function here
+
+    # Speed up the tests by only running this test on all input graphs
+    # The remaining tests run only on graph1
+
+    """
+    Run Random walk with restart in the Docker image
+    """
+    def test_rwr_required(self):
+        out_path = Path(OUT_FILES_1)
+        out_path.unlink(missing_ok=True)
+        # Only include required arguments
+        RandomWalk.run(sources=TEST_DIR + 'input/'  + '/source_nodes.txt',
+                        targets=TEST_DIR + 'input/' + '/target_nodes.txt',
+                        edges=TEST_DIR + 'input/' + '/edges.txt',
+                        output_file=OUT_FILES_1)
+        assert out_path.exists()
+        
+    def test_rwr_alternative_graph(self):
+        out_path = Path(OUT_FILES_2)
+        out_path.unlink(missing_ok=True)
+        RandomWalk.run(sources=TEST_DIR + 'input/'  + '/source_nodes1.txt',
+                        targets=TEST_DIR + 'input/' + '/target_nodes1.txt',
+                        edges=TEST_DIR + 'input/' + '/edges1.txt',
+                        output_file=OUT_FILES_2)
+        assert out_path.exists()
+        
+    def test_rwr_some_optional(self):
+        out_path = Path(OUT_FILES_1)
+        out_path.unlink(missing_ok=True)
+        # Include optional argument
+        RandomWalk.run(sources=TEST_DIR + 'input/'  + '/source_nodes.txt',
+                        targets=TEST_DIR + 'input/' + '/target_nodes.txt',
+                        edges=TEST_DIR + 'input/' + '/edges.txt',
+                        output_file=OUT_FILES_1,
+                        df = '0.7')
+        assert out_path.exists()
+        
+    def test_rwr_all_optional(self):
+        out_path = Path(OUT_FILES_1)
+        out_path.unlink(missing_ok=True)
+        # Include all optional arguments
+        RandomWalk.run(sources=TEST_DIR + 'input/'  + '/source_nodes.txt',
+                        targets=TEST_DIR + 'input/' + '/target_nodes.txt',
+                        edges=TEST_DIR + 'input/' + '/edges.txt',
+                        output_file=OUT_FILES_1,
+                        df = '0.7',
+                        f = 'sum')
+        assert out_path.exists()
+        
+
+    def test_rw_missing(self):
+        # Test the expected error is raised when required arguments are missing
+        with pytest.raises(ValueError):
+            # No edges file
+            RandomWalk.run(sources=TEST_DIR + 'input/'  + '/source_nodes.txt',
+                            targets=TEST_DIR + 'input/' + '/target_nodes.txt',
+                            output_file=OUT_FILES_1)
+
+    @pytest.mark.skipif(not shutil.which('singularity'), reason='Singularity not found on system')
+    def test_rwr_singularity(self):
+        out_path = Path(OUT_FILES_1)
+        out_path.unlink(missing_ok=True)
+        # Only include required arguments and run with Singularity
+        RandomWalk.run(sources=TEST_DIR + 'input/'  + '/source_nodes.txt',
+                        targets=TEST_DIR + 'input/' + '/target_nodes.txt',
+                        edges=TEST_DIR + 'input/' + '/edges.txt',
+                        output_file=OUT_FILES_1,
+                        df = '0.7',
+                        f = 'sum',
+                        singularity=True)
+        assert out_path.exists()
