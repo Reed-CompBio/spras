90a38b3c8983a2a08ab669cab98cc8b2bc61a28c
ntalluri <nehatalluri@live.com>
2023-07-17 20:08:16 -0700

    added correctness test

--- test/AllPairs/test_ap.py
+++ test/AllPairs/test_ap.py
@@ -1,9 +1,6 @@
 import shutil
 from pathlib import Path
-
-import docker
 import pytest
-
 from src.allpairs import AllPairs
 
 TEST_DIR = 'test/AllPairs/'
@@ -26,13 +23,14 @@ class TestAllPairs:
         )
         assert out_path.exists()
 
-    def test_pathlinker_missing(self):
+    def test_allpairs_missing(self):
         # Test the expected error is raised when required arguments are missing
         with pytest.raises(ValueError):
             # No nodetypes
             AllPairs.run(
                 network=TEST_DIR + 'input/sample-in-net.txt',
                 output_file=OUT_FILE)
+
 
     # Only run Singularity test if the binary is available on the system
     # spython is only available on Unix, but do not explicitly skip non-Unix platforms
@@ -48,3 +46,37 @@ class TestAllPairs:
             singularity=True
         )
         assert out_path.exists()
+
+    def test_correctness(self):
+        """
+        Tests algorithm correctness of all_pairs_shortest_path.py by using AllPairs.run
+        """
+        out_path = Path(OUT_FILE)
+        out_path.unlink(missing_ok=True)
+        
+        AllPairs.run(
+            nodetypes=TEST_DIR+'input/correctness-nodetypes.txt',
+            network=TEST_DIR+'input/correctness-network.txt',
+            output_file=OUT_FILE
+        )
+
+        assert out_path.exists()
+
+        with open(out_path, 'r') as f:
+            edge_pairs = f.readlines()
+        output_edges = []
+        for edge in edge_pairs:
+            node1, node2 = sorted(edge.split())
+            output_edges.append((node1, node2))
+        output_edges.sort()
+
+        expected_output = [
+            ('A', 'B'),
+            ('B', 'C'),
+            ('C', 'D'),
+        ]
+
+        assert output_edges == expected_output
+
+
+
