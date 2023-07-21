b2d6a8e3185bf37318097743795f324e93f9749c
Erik_L <liue@reed.edu>
2023-07-20 18:34:46 -0700

    Updated tests for RWR

--- test/RWR/test_rwr.py
+++ test/RWR/test_rwr.py
@@ -19,43 +19,44 @@ class TestRWR:
         out_path = Path(OUT_FILES_1)
         out_path.unlink(missing_ok=True)
         # Only include required arguments
-        RWR.run(sources=TEST_DIR + 'input/'  + '/source_nodes.txt',
-                        targets=TEST_DIR + 'input/' + '/target_nodes.txt',
-                        edges=TEST_DIR + 'input/' + '/edges.txt',
+        RWR.run(edges=TEST_DIR + 'input/' + '/edges.txt',
+                        single_source='1',
+                        prizes=TEST_DIR + 'input/' + '/prizes.txt',
                         output_file=OUT_FILES_1)
         assert out_path.exists()
 
     def test_rwr_alternative_graph(self):
         out_path = Path(OUT_FILES_2)
         out_path.unlink(missing_ok=True)
-        RWR.run(sources=TEST_DIR + 'input/'  + '/source_nodes1.txt',
-                        targets=TEST_DIR + 'input/' + '/target_nodes1.txt',
-                        edges=TEST_DIR + 'input/' + '/edges1.txt',
+        RWR.run(edges=TEST_DIR + 'input/' + '/edges1.txt',
+                        single_source='1',
+                        prizes=TEST_DIR + 'input/' + '/prizes1.txt',
                         output_file=OUT_FILES_2)
         assert out_path.exists()
 
-    def test_rwr_some_optional(self):
-        out_path = Path(OUT_FILES_1)
+    def test_rwr_alternative_graph(self):
+        out_path = Path(OUT_FILES_2)
         out_path.unlink(missing_ok=True)
-        # Include optional argument
-        RWR.run(sources=TEST_DIR + 'input/'  + '/source_nodes.txt',
-                        targets=TEST_DIR + 'input/' + '/target_nodes.txt',
-                        edges=TEST_DIR + 'input/' + '/edges.txt',
-                        output_file=OUT_FILES_1,
-                        df = 0.7)
+        RWR.run(edges=TEST_DIR + 'input/' + '/edges1.txt',
+                        single_source='0',
+                        prizes=TEST_DIR + 'input/' + '/prizes1.txt',
+                        df = 0.8,
+                        w = 0.3,
+                        output_file=OUT_FILES_2)
         assert out_path.exists()
 
     def test_rwr_all_optional(self):
         out_path = Path(OUT_FILES_1)
         out_path.unlink(missing_ok=True)
         # Include all optional arguments
-        RWR.run(sources=TEST_DIR + 'input/'  + '/source_nodes.txt',
-                        targets=TEST_DIR + 'input/' + '/target_nodes.txt',
-                        edges=TEST_DIR + 'input/' + '/edges.txt',
+        RWR.run(edges=TEST_DIR + 'input/' + '/edges1.txt',
+                        single_source='0',
+                        prizes=TEST_DIR + 'input/' + '/prizes1.txt',
                         output_file=OUT_FILES_1,
                         df = 0.8,
                         f = 'sum',
-                        w = 0.3)
+                        w = 0.3,
+                        threshold= 0.1)
         assert out_path.exists()
 
 
@@ -63,8 +64,7 @@ class TestRWR:
         # Test the expected error is raised when required arguments are missing
         with pytest.raises(ValueError):
             # No edges file
-            RWR.run(sources=TEST_DIR + 'input/'  + '/source_nodes.txt',
-                            targets=TEST_DIR + 'input/' + '/target_nodes.txt',
+            RWR.run(single_source='1',
                             output_file=OUT_FILES_1)
 
     @pytest.mark.skipif(not shutil.which('singularity'), reason='Singularity not found on system')
@@ -72,11 +72,8 @@ class TestRWR:
         out_path = Path(OUT_FILES_1)
         out_path.unlink(missing_ok=True)
         # Only include required arguments and run with Singularity
-        RWR.run(sources=TEST_DIR + 'input/'  + '/source_nodes.txt',
-                        targets=TEST_DIR + 'input/' + '/target_nodes.txt',
-                        edges=TEST_DIR + 'input/' + '/edges.txt',
-                        output_file=OUT_FILES_1,
-                        df = '0.7',
-                        f = 'sum',
-                        singularity=True)
+        RWR.run(edges=TEST_DIR + 'input/' + '/edges1.txt',
+                        single_source='1',
+                        prizes=TEST_DIR + 'input/' + '/prizes1.txt',
+                        output_file=OUT_FILES_2)
         assert out_path.exists()
