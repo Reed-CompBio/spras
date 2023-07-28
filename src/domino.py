9d42677ed8cedfa033ad73c01998f3862fa117fe
livj4711 <4711.liv@gmail.com>
2023-07-28 11:37:55 -0500

    transform gene ids in domino.py

--- src/domino.py
+++ src/domino.py
@@ -9,6 +9,10 @@ from src.util import prepare_volume, run_container
 
 __all__ = ['DOMINO']
 
+PERIOD_SUB = '♥' # U+2665
+ID_PREFIX = 'ENSG0'
+ID_SUFFIX = '☺.1'
+
 class DOMINO(PRM):
     required_inputs = ['network', 'active_genes']
 
@@ -24,20 +28,32 @@ class DOMINO(PRM):
             if input_type not in filename_map:
                 raise ValueError(f"{input_type} filename is missing")
 
-        #Get active genes for node input file
+        # Get active genes for node input file
         if data.contains_node_columns('active'):
             #NODEID is always included in the node table
             node_df = data.request_node_columns(['active'])
         else:
             raise ValueError("DOMINO requires active genes")
+        node_df = node_df[node_df['active'] == True]
+
+        # Replace periods in each node id with PERIOD_SUB and transform with a prefix and suffix
+        node_df['NODEID'] = node_df['NODEID'].apply(pre_domino_id_transform)
+        # e.g., ENSG0[node_id♥]☺.1
 
         #Create active_genes file
         node_df.to_csv(filename_map['active_genes'],sep="\t",index=False,columns=['NODEID'], header=False)
 
+
         #Create network file
         edges_df = data.get_interactome()
         edges_df['ppi'] = 'ppi'
+
+        # Replace periods in each node id with PERIOD_SUB and transform with a prefix and suffix
+        edges_df['Interactor1'] = edges_df['Interactor1'].apply(pre_domino_id_transform)
+        edges_df['Interactor2'] = edges_df['Interactor2'].apply(pre_domino_id_transform)
+
         edges_df.to_csv(filename_map['network'],sep='\t',index=False,columns=['Interactor1','ppi','Interactor2'],header=['ID_interactor_A','ppi','ID_interactor_B'])
+
 
 
     @staticmethod
@@ -96,7 +112,6 @@ class DOMINO(PRM):
 
         ########
 
-
         # Make the Python command to run within the container
         command = ['domino',
                    '--active_genes_files', node_file,
@@ -122,22 +137,29 @@ class DOMINO(PRM):
                             command,
                             volumes,
                             work_dir)
-        print(out)
-
+        #print(out)
 
         ########
 
-        Path(mapped_slices_dir).unlink(missing_ok=True)
-        Path(out_dir, 'modules.out').unlink(missing_ok=True)
+        # Path(mapped_slices_dir).unlink(missing_ok=True)
+        # Path(out_dir, 'modules.out').unlink(missing_ok=True)
         #for domino_output in out_dir.glob('modules.out'):
         #    domino_output.unlink(missing_ok=True)
 
+        # domino creates a new folder in mapped_out_dir to output its modules files into
+        out_modules = mapped_out_dir + '/active_genes'
+
         # concatenate each module html file into one big file
         with open(output_file, "w") as fo:
-            for html_file in out_dir.glob('module_*.html'):
+            for html_file in Path(out_modules).glob('module_*.html'):
                 with open(html_file,'r') as fi:
                     fo.write(fi.read())
-                Path(html_file).unlink(missing_ok=True)
+                # Path(html_file).unlink(missing_ok=True)
+
+
+
+
+
 
 
     @staticmethod
@@ -149,6 +171,8 @@ class DOMINO(PRM):
         """
         edges = pd.DataFrame()
 
+        print("##############")
+        print("rawpathways:", raw_pathway_file)
         with open(raw_pathway_file, 'r') as file:
             for line in file:
                 if line.strip().startswith("let data = ["):
@@ -161,13 +185,40 @@ class DOMINO(PRM):
                     for entry in data:
                         tmp = entry['data']
                         entries.append(tmp)
+                        print("tmp:", tmp)
 
                     df = pd.DataFrame(entries)
+                    print("df:", df)
                     newdf = df.loc[:,['source', 'target']].dropna()
+                    print("newdf:", newdf)
 
                     edges = pd.concat([edges, newdf], axis=0)
+        print("edges:", edges)
 
         edges['rank'] = 1 # adds in a rank column of 1s because the edges are not ranked
 
+        # Remove the prefix and unicode of suffix only, restore the period
+        edges['source'] = edges['source'].apply(post_domino_id_transform)
+        edges['target'] = edges['target'].apply(post_domino_id_transform)
+
         edges.to_csv(standardized_pathway_file, header=False, index=False)
+
+
+def pre_domino_id_transform(node_id):
+    """
+    Replace periods with PERIOD_SUB, prepend each node id with ID_PREFIX and append each node with ID_SUFFIX
+    @param node_id: the node id to transformed
+    """
+    node_id = node_id.replace('.', PERIOD_SUB)
+    return ID_PREFIX + node_id + ID_SUFFIX
+
+
+def post_domino_id_transform(node_id):
+    """
+    Remove prefix and suffix, replace PERIOD_SUB with .
+    @param node_id: the node id to transformed
+    """
+    node_id = node_id.str[5:-1]
+    node_id = node_id.str.replace(PERIOD_SUB, '.')
+    return node_id
 
