a705a919302acc793ee5379399b2aa6e50298c36
ntalluri <nehatalluri@live.com>
2023-08-17 13:22:42 -0500

    Revert "added deterministic test and fixed naming"
    This reverts commit a257f749f3e3f459fbd6de3bb0500db55478cc80.

--- test/AllPairs/test_ap.py
+++ test/AllPairs/test_ap.py
@@ -6,22 +6,22 @@ import pytest
 from src.allpairs import AllPairs
 
 TEST_DIR = 'test/AllPairs/'
-OUT_DIR = TEST_DIR+'output/'
+OUT_FILE = TEST_DIR+'output/out.txt'
 
-EXPECTED_DIR = TEST_DIR+'/expected/' 
+EXPECTED_FILE = TEST_DIR+'/expected/out.txt' ## TODO not currently checked.
 
 class TestAllPairs:
     """
     Run all pairs shortest paths (AllPairs) tests in the Docker image
     """
     def test_allpairs(self):
-        out_path = Path(OUT_DIR+'sample-out.txt')
+        out_path = Path(OUT_FILE)
         out_path.unlink(missing_ok=True)
         # Only include required arguments
         AllPairs.run(
             nodetypes=TEST_DIR+'input/sample-in-nodetypes.txt',
             network=TEST_DIR+'input/sample-in-net.txt',
-            output_file=OUT_DIR+'sample-out.txt'
+            output_file=OUT_FILE
         )
         assert out_path.exists()
 
@@ -31,20 +31,20 @@ class TestAllPairs:
             # No nodetypes
             AllPairs.run(
                 network=TEST_DIR + 'input/sample-in-net.txt',
-                output_file=OUT_DIR+'sample-out.txt')
+                output_file=OUT_FILE)
 
 
     # Only run Singularity test if the binary is available on the system
     # spython is only available on Unix, but do not explicitly skip non-Unix platforms
     @pytest.mark.skipif(not shutil.which('singularity'), reason='Singularity not found on system')
     def test_allpairs_singularity(self):
-        out_path = Path(OUT_DIR+'sample-out.txt')
+        out_path = Path(OUT_FILE)
         out_path.unlink(missing_ok=True)
         # Only include required arguments and run with Singularity
         AllPairs.run(
             nodetypes=TEST_DIR+'input/sample-in-nodetypes.txt',
             network=TEST_DIR+'input/sample-in-net.txt',
-            output_file=OUT_DIR+'sample-out.txt',
+            output_file=OUT_FILE,
             singularity=True
         )
         assert out_path.exists()
@@ -53,13 +53,13 @@ class TestAllPairs:
         """
         Tests algorithm correctness of all_pairs_shortest_path.py by using AllPairs.run
         """
-        out_path = Path(OUT_DIR+'correctness.txt')
+        out_path = Path(OUT_FILE)
         out_path.unlink(missing_ok=True)
 
         AllPairs.run(
             nodetypes=TEST_DIR+'input/correctness-nodetypes.txt',
             network=TEST_DIR+'input/correctness-network.txt',
-            output_file=OUT_DIR+'correctness.txt'
+            output_file=OUT_FILE
         )
 
         assert out_path.exists()
@@ -72,15 +72,13 @@ class TestAllPairs:
             output_edges.append((node1, node2))
         output_edges.sort()
 
-        with open(EXPECTED_DIR+"correctness-expected.txt", 'r') as file:
-            correctness_edge_pairs = file.readlines()
-        correctness_edges = []
-        for edge in correctness_edge_pairs:
-            node1, node2 = edge.split()
-            correctness_edges.append((node1, node2))
+        expected_output = [
+            ('A', 'B'),
+            ('B', 'C'),
+            ('C', 'D'),
+        ]
 
-        assert output_edges == correctness_edges
-
+        assert output_edges == expected_output
 
 
 
