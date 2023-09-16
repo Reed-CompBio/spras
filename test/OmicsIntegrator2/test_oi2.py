cc1f1046ec400b551c5b5c126241ceb973f96b1d
Anthony Gitter <gitter@biostat.wisc.edu>
2023-09-15 22:21:49 -0500

    Run Singularity test with Singularity

--- test/OmicsIntegrator2/test_oi2.py
+++ test/OmicsIntegrator2/test_oi2.py
@@ -6,6 +6,8 @@ import pytest
 from src.omicsintegrator2 import OmicsIntegrator2
 
 TEST_DIR = 'test/OmicsIntegrator2/'
+EDGE_FILE = TEST_DIR+'input/oi2-edges.txt'
+PRIZE_FILE = TEST_DIR+'input/oi2-prizes.txt'
 OUT_FILE = Path(TEST_DIR, 'output', 'test.tsv')
 
 
@@ -16,16 +18,16 @@ class TestOmicsIntegrator2:
     def test_oi2_required(self):
         # Only include required arguments
         OUT_FILE.unlink(missing_ok=True)
-        OmicsIntegrator2.run(edges=TEST_DIR+'input/oi2-edges.txt',
-                             prizes=TEST_DIR+'input/oi2-prizes.txt',
+        OmicsIntegrator2.run(edges=EDGE_FILE,
+                             prizes=PRIZE_FILE,
                              output_file=OUT_FILE)
         assert OUT_FILE.exists()
 
     def test_oi2_some_optional(self):
         # Include optional argument
         OUT_FILE.unlink(missing_ok=True)
-        OmicsIntegrator2.run(edges=TEST_DIR+'input/oi2-edges.txt',
-                             prizes=TEST_DIR+'input/oi2-prizes.txt',
+        OmicsIntegrator2.run(edges=EDGE_FILE,
+                             prizes=PRIZE_FILE,
                              output_file=OUT_FILE,
                              g=0)
         assert OUT_FILE.exists()
@@ -33,8 +35,8 @@ class TestOmicsIntegrator2:
     def test_oi2_all_optional(self):
         # Include all optional arguments
         OUT_FILE.unlink(missing_ok=True)
-        OmicsIntegrator2.run(edges=TEST_DIR+'input/oi2-edges.txt',
-                             prizes=TEST_DIR+'input/oi2-prizes.txt',
+        OmicsIntegrator2.run(edges=EDGE_FILE,
+                             prizes=PRIZE_FILE,
                              output_file=OUT_FILE,
                              w=5,
                              b=1,
@@ -43,15 +45,16 @@ class TestOmicsIntegrator2:
                              noisy_edges=0,
                              random_terminals=0,
                              dummy_mode='terminals',
-                             seed=2)
+                             seed=2,
+                             singularity=False)
         assert OUT_FILE.exists()
 
     def test_oi2_missing(self):
         # Test the expected error is raised when required arguments are missing
         with pytest.raises(ValueError):
             # No output_file
-            OmicsIntegrator2.run(edges=TEST_DIR+'input/oi2-edges.txt',
-                                 prizes=TEST_DIR+'input/oi2-prizes.txt')
+            OmicsIntegrator2.run(edges=EDGE_FILE,
+                                 prizes=PRIZE_FILE)
 
     # Only run Singularity test if the binary is available on the system
     # spython is only available on Unix, but do not explicitly skip non-Unix platforms
@@ -59,7 +62,8 @@ class TestOmicsIntegrator2:
     def test_oi2_singularity(self):
         # Only include required arguments
         OUT_FILE.unlink(missing_ok=True)
-        OmicsIntegrator2.run(edges=TEST_DIR+'input/oi2-edges.txt',
-                             prizes=TEST_DIR+'input/oi2-prizes.txt',
-                             output_file=OUT_FILE)
+        OmicsIntegrator2.run(edges=EDGE_FILE,
+                             prizes=PRIZE_FILE,
+                             output_file=OUT_FILE,
+                             singularity=True)
         assert OUT_FILE.exists()
