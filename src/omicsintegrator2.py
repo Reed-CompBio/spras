713279ac5464f027402709d36a0ed90785385fef
ntalluri <nehatalluri@live.com>
2023-09-18 12:32:50 -0500

    pre-commit

--- src/omicsintegrator2.py
+++ src/omicsintegrator2.py
@@ -2,11 +2,11 @@ from pathlib import Path
 
 import pandas as pd
 
+from src.dataset import Dataset
 from src.interactome import (
     convert_directed_to_undirected,
     readd_direction_col_undirected,
 )
-from src.dataset import Dataset
 from src.prm import PRM
 from src.util import add_rank_column, prepare_volume, run_container
 
