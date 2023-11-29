e8735ea70119f3e36f9cb3802592081aa9896cf7
ntalluri <nehatalluri@live.com>
2023-11-29 15:43:43 -0600

    resolving parse output comments

--- test/parse-outputs/test_parse_outputs.py
+++ test/parse-outputs/test_parse_outputs.py
@@ -12,12 +12,7 @@ INDIR = "test/parse-outputs/input/"
 OUTDIR = "test/parse-outputs/output/"
 EXPDIR = "test/parse-outputs/expected/"
 
-# for domino
-OUT_FILE_PARSE = OUTDIR+'/domino-parse-output.txt'
-OUT_FILE_PARSE_EXP = EXPDIR + '/domino-parse-output.txt'
-
-# domino is seperate function
-algorithms = ['mincostflow', 'meo', 'omicsintegrator1', 'omicsintegrator2', 'pathlinker', 'allpairs']
+algorithms = ['mincostflow', 'meo', 'omicsintegrator1', 'omicsintegrator2', 'pathlinker', 'allpairs', 'domino']
 class TestParseOutputs:
     def setup_class(cls):
         """
@@ -29,18 +24,6 @@ class TestParseOutputs:
         for algo in algorithms:
             test_file = INDIR + f"{algo}-raw-pathway.txt"
             out_file = OUTDIR + f"{algo}-pathway.txt"
-            print(out_file)
+            
             runner.parse_output(algo, test_file, out_file)
             assert filecmp.cmp(OUTDIR +f"{algo}-pathway.txt", EXPDIR + f"{algo}-pathway-expected.txt", shallow=False)
-
-    def test_domino_parse_output(self):
-        # Input is the concatenated module_0.html and module_1.html file from
-        # the DOMINO output of the network dip.sif and the nodes tnfa_active_genes_file.txt
-        # from https://github.com/Shamir-Lab/DOMINO/tree/master/examples
-        # Confirms the generated output matches the expected output
-        out_path = Path(OUT_FILE_PARSE)
-        out_path.unlink(missing_ok=True)
-        DOMINO.parse_output(
-            INDIR+ 'domino-concat-modules.txt',
-            OUT_FILE_PARSE)
-        assert filecmp.cmp(OUT_FILE_PARSE, OUT_FILE_PARSE_EXP, shallow=False)
