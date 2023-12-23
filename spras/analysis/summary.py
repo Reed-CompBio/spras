5edf8a79fceabcb542d2c5eb727ef6ed32ee2627
ntalluri <nehatalluri@live.com>
2023-12-23 14:57:54 -0800

    updated summary.py code

--- spras/analysis/summary.py
+++ spras/analysis/summary.py
@@ -33,16 +33,12 @@ def summarize_networks(file_paths: Iterable[Path], node_table: pd.DataFrame) -> 
 
     # Iterate through each network file path
     for file_path in sorted(file_paths):
-        nw = None
-        # nw = nx.read_edgelist(file_path, data=(('weight', float), ('Direction',str)))
-        if os.path.getsize(file_path) == 0:
-            continue
-        else:
-            with open(file_path, 'r') as f:
-                # skip the header line
-                next(f)
-                # Load in the network
-                nw = nx.read_edgelist(f, data=(('weight', float), ('Direction', str)))
+        
+        lines = None
+        with open(file_path, 'r') as f:
+            lines = f.readlines()[1:]  # skip the first line
+       
+        nw = nx.read_edgelist(lines, data=(('weight', float), ('Direction', str)))
 
         # Save the network name, number of nodes, number edges, and number of connected components
         nw_name = str(file_path)
