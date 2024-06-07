fe47da597036dcdf7af0c6920502476897a134ac
gabeah <gabeh@reed.edu>
2024-06-07 15:11:23 -0700

    dealing with some errors

--- spras/local_neighborhood.py
+++ spras/local_neighborhood.py
@@ -8,6 +8,7 @@ from spras.interactome import (
     convert_undirected_to_directed,
     reinsert_direction_col_directed,
 )
+from spras.util import add_rank_column
 from spras.prm import PRM
 
 __all__ = ['LocalNeighborhood']
@@ -23,34 +24,105 @@ class LocalNeighborhood:
         @param filename_map:
         @return:
         """
-        
+        print('generating inputs!!')
         # Check if filename
         for input_type in LocalNeighborhood.required_inputs:
             if input_type not in filename_map:
                 raise ValueError(f"{input_type} filename is missing")
 
-        if data.contains_node_columns('prize'):
-            print("h")
-        # Omics example
-        if data.contains_node_columns('prize'):
-
-            node_df = data.request_node_columns(['prize'])
-        elif data.contains_node_columns('sources'):
-
-            node_df = data.request_node_columns(['sources','targets'])
-            node_df.loc[node_df['sources']==True, 'prize'] = 1.0
-            node_df.loc[node_df['targets']==True, 'prize'] = 1.0
+        # Select nodes that have sources, targets, prizes, or are active
+        if data.contains_node_columns(['sources','targets','prize']):
+            node_df = data.request_node_columns(['sources','targets','prize'])
 
         else:
             raise ValueError("LocalNeighborhood requires nore prizes or sources and targets")
 
         # LocalNeighborhood already gives warnings
-        node_df.to_csv(filename_map['prizes'],sep='\t', index = False, columns=['NODEID','prize'],header=['name','prize'])
+        node_df.to_csv(filename_map['nodes'],
+                        #sep='\t', 
+                        index = False, 
+                        columns=['NODEID'],
+                        header=False)
 
         # Get network file
         edges_df = data.get_interactome()
 
         # Rename Direction column
-        edges_df.to_csv(filename_map['edges'],sep='\t',index=False,
-                        columns=['Interactor1','Interactor2','Weight','Direction'],
-                        header=['protein1','protein2','weight','directionality'])
+        edges_df.to_csv(filename_map['network'],
+                        sep='|',
+                        index=False,
+                        columns=['Interactor1','Interactor2'],
+                        header=False)
+        
+    @staticmethod
+    def run(nodes=None, network=None, output_file=None, container_framework="docker"):
+        '''
+        Method to running LocalNeighborhood correctly
+        @param nodes:  input node types with sources and targets (required)
+        @param network:  input network file (required)
+        @param output_file: path to the output pathway file (required)
+        '''
+        print('Running!!!')
+        if not nodes or not network or not output_file:
+            raise ValueError('Required LocalNeighborhood arguments are missing')
+
+        work_dir = '/spras'
+
+        volumes = list()
+
+        bind_path, node_file = prepare_volume(nodes, work_dir)
+        volumes.append(bind_path)
+
+        bind_path, network_file = prepare_volume(network, work_dir)
+        volumes.append(bind_path)
+
+        # LocalNeighborhood does not provide an argument to set the output directory
+        # Use its --output argument to set the output file prefix to specify an absolute path and prefix
+        out_dir = Path(output_file).parent
+        # LocalNeighborhood requires that the output directory exist
+        out_dir.mkdir(parents=True, exist_ok=True)
+        bind_path, mapped_out_dir = prepare_volume(str(out_dir), work_dir)
+        volumes.append(bind_path)
+        mapped_out_prefix = mapped_out_dir + '/out'  # Use posix path inside the container
+
+        command = ['python',
+                   '/LocalNeighborhood/local_neighborhood.py',
+                   '--network', network_file,
+                   '--nodes', node_file,
+                   '--output', mapped_out_prefix]
+
+        print('Running LocalNeighborhood with arguments: {}'.format(' '.join(command)), flush=True)
+
+        container_suffix = "local-neighborhood"
+        out = run_container(container_framework,
+                            container_suffix,
+                            command,
+                            volumes,
+                            work_dir)
+        print(out)
+
+        # Rename the primary output file to match the desired output filename
+        # Currently LocalNeighborhood only writes one output file so we do not need to delete others
+        output_edges = Path(out_dir, 'out')
+        output_edges.rename(output_file)
+
+    @staticmethod
+    def parse_output(raw_pathway_file, standardized_pathway_file):
+        '''
+        Method for standardizing output data
+        @raw_pathway_file: raw output from LocalNeighborhood
+        @standardized_pathway_file: universal output, for use in Pandas analysis
+        '''
+        print('Parsing outputs!!')
+        df = pd.read_csv(raw_pathway_file, 
+            sep='|',
+            header=None
+            )
+        print(df)
+        df = add_rank_column(df)
+        print(df)
+        print(df.to_csv(header=False,index=False,sep='\t'))
+        df.to_csv(standardized_pathway_file, 
+                    header=None, 
+                    index=False, 
+                    sep='\t')
--- spras/runner.py
+++ spras/runner.py
@@ -2,6 +2,7 @@
 from spras.allpairs import AllPairs as allpairs
 from spras.dataset import Dataset
 from spras.domino import DOMINO as domino
+from spras.local_neighborhood import LocalNeighborhood as local_neighborhood
 from spras.meo import MEO as meo
 from spras.mincostflow import MinCostFlow as mincostflow
 from spras.omicsintegrator1 import OmicsIntegrator1 as omicsintegrator1
