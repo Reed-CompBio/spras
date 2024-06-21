a59e281107d440a001540fd1ef0bdbbcbd97dd5d
gabeah <gabeh@reed.edu>
2024-06-21 15:29:59 -0700

    working on btb pr

--- spras/runner.py
+++ spras/runner.py
@@ -8,6 +8,7 @@ from spras.mincostflow import MinCostFlow as mincostflow
 from spras.omicsintegrator1 import OmicsIntegrator1 as omicsintegrator1
 from spras.omicsintegrator2 import OmicsIntegrator2 as omicsintegrator2
 from spras.pathlinker import PathLinker as pathlinker
+from spras.btb import BowtieBuilder as btb
 
 
 def run(algorithm, params):
