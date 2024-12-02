ed6e4caa28b65e0d668839cebfca374d2c641036
Carol <cgsze2@gmail.com>
2024-12-02 15:48:02 -0600

    edit test cases

--- test/analysis/test_summary.py
+++ test/analysis/test_summary.py
@@ -1,8 +1,16 @@
 from pathlib import Path
-
 import pandas as pd
-
 from spras.analysis.summary import summarize_networks
+
+# set up necessary dataframes to run summarize_networks
+import spras.config as _config
+
+_config.init_global(config)
+algorithm_params = _config.config.algorithm_params
+algorithm_directed = _config.config.algorithm_directed
+
+algorithms = list(algorithm_params)
+algorithms_with_params = [f'{algorithm}-params-{params_hash}' for algorithm, param_combos in algorithm_params.items() for params_hash in param_combos.keys()]
 
 # Notes:
 # - Column labels are required in the node table
@@ -11,21 +19,13 @@ from spras.analysis.summary import summarize_networks
 
 
 class TestSummary:
-	# Toy networks test case not in use since it does not have a config file
-	#def test_toy_networks(self):
-	#	toy_network_files = Path("test/analysis/input/toy").glob("*.txt")
-	#	toy_node_table = pd.read_csv(Path("test/analysis/input/toy_node_table.txt"), sep = "\t")
-	#	toy_output = pd.read_csv(Path("test/analysis/output/toy_summary.txt"), sep = "\t")
-	#	toy_output["Name"] = toy_output["Name"].map(convert_path)
-	#	assert summarize_networks(toy_network_files, toy_node_table).equals(toy_output)
-
 	# Test data from example workflow:
 	def test_example_networks(self):
 		example_network_files = Path("test/analysis/input/example").glob("*.txt")
 		example_node_table = pd.read_csv(Path("test/analysis/input/example_node_table.txt"), sep = "\t")
 		example_output = pd.read_csv(Path("test/analysis/output/example_summary.txt"), sep = "\t")
 		example_output["Name"] = example_output["Name"].map(convert_path)
-		assert summarize_networks(example_network_files, example_node_table).equals(example_output)
+		assert summarize_networks(example_network_files, example_node_table, algorithm_params, algorithms_with_params).equals(example_output)
 
 	# Test data from EGFR workflow:
 	def test_egfr_networks(self):
@@ -33,7 +33,7 @@ class TestSummary:
 		egfr_node_table = pd.read_csv(Path("test/analysis/input/egfr_node_table.txt"), sep = "\t")
 		egfr_output = pd.read_csv(Path("test/analysis/output/egfr_summary.txt"), sep = "\t")
 		egfr_output["Name"] = egfr_output["Name"].map(convert_path)
-		assert summarize_networks(egfr_network_files, egfr_node_table).equals(egfr_output)
+		assert summarize_networks(egfr_network_files, egfr_node_table, algorithm_params, algorithms_with_params).equals(egfr_output)
 
 
 # File paths have to be converted for the stored expected output files because otherwise the dataframes may not
