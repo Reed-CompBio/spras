9a4ccae1da96a80976c85ee934654e774186f697
ntalluri <nehatalluri@live.com>
2025-03-03 13:48:55 -0600

    pre-commit

--- spras/mincostflow.py
+++ spras/mincostflow.py
@@ -141,7 +141,7 @@ class MinCostFlow (PRM):
         Convert a predicted pathway into the universal format
 
         The algorithm constructs a mixed directional network, the resulting network is treated as mixed directional.
-        
+
         @param raw_pathway_file: pathway file produced by an algorithm's run function
         @param standardized_pathway_file: the same pathway written in the universal format
         """
