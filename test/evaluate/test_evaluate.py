61c348219b73f33cd3a058338b6bb43576e6a9a3
ntalluri <nehatalluri@live.com>
2025-03-03 16:01:50 -0600

    update test cases

--- test/evaluate/test_evaluate.py
+++ test/evaluate/test_evaluate.py
@@ -19,46 +19,6 @@ class TestEvaluate:
         """
         Path(OUT_DIR).mkdir(parents=True, exist_ok=True)
 
-    def test_node_ensemble(self):
-        ensemble_file = INPUT_DIR + 'ensemble-network.tsv'
-        edge_freq = Evaluation.edge_frequency_node_ensemble(ensemble_file)
-        edge_freq.to_csv(OUT_DIR + 'node-ensemble.csv', sep="\t", index=False)
-        assert filecmp.cmp(OUT_DIR + 'node-ensemble.csv', EXPECT_DIR + 'expected-node-ensemble.csv', shallow=False)
-
-    def test_precision_recal_curve_ensemble_nodes(self):
-        out_path = Path(OUT_DIR+"test-precision-recall-curve-ensemble-nodes.png")
-        out_path.unlink(missing_ok=True)
-        ensemble_file = pd.read_csv(INPUT_DIR + 'node-ensemble.csv', sep="\t", header=0)
-        Evaluation.precision_recall_curve_node_ensemble(ensemble_file, NODE_TABLE, out_path)
-        assert out_path.exists()
-
-    def test_precision_recal_curve_ensemble_nodes_empty(self):
-        out_path = Path(OUT_DIR+"test-precision-recall-curve-ensemble-nodes-empty.png")
-        out_path.unlink(missing_ok=True)
-        ensemble_file = pd.read_csv(INPUT_DIR + 'node-ensemble-empty.csv', sep="\t", header=0)
-        Evaluation.precision_recall_curve_node_ensemble(ensemble_file, NODE_TABLE, out_path)
-        assert out_path.exists()
-
-    def test_precision_recall_per_pathway(self):
-        file_paths = [INPUT_DIR + "data-test-params-123/pathway.txt", INPUT_DIR + "data-test-params-456/pathway.txt",  INPUT_DIR + "data-test-params-789/pathway.txt",  INPUT_DIR + "data-test-params-empty/pathway.txt"]
-        algorithms = ["test"]
-        output_file = OUT_DIR + "test-precision-recall-per-pathway.txt"
-        output_png = OUT_DIR + "test-precision-recall-per-pathway.png"
-
-        Evaluation.precision_and_recall(file_paths, NODE_TABLE, algorithms, output_file, output_png)
-        assert filecmp.cmp(output_file, EXPECT_DIR + 'expected-precision-recall-per-pathway.txt', shallow=False)
-
-    def test_precision_recall_per_pathway_empty(self):
-
-        file_paths = [INPUT_DIR + "data-test-params-empty/pathway.txt"]
-        algorithms = ["test"]
-        output_file = OUT_DIR +"test-precision-recall-per-pathway-empty.txt"
-        output_png = OUT_DIR + "test-precision-recall-per-pathway-empty.png"
-
-        Evaluation.precision_and_recall(file_paths, NODE_TABLE, algorithms, output_file, output_png)
-        assert filecmp.cmp(output_file, EXPECT_DIR + 'expected-precision-recall-per-pathway-empty.txt', shallow=False)
-
-
     def  test_precision_recall_pca_chosen_pathway(self):
         file_paths = [INPUT_DIR + "data-test-params-123/pathway.txt", INPUT_DIR + "data-test-params-456/pathway.txt",  INPUT_DIR + "data-test-params-789/pathway.txt",  INPUT_DIR + "data-test-params-empty/pathway.txt"]
         algorithms = ["test"]
