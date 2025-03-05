a1418cf1e201c3adf37f9de989269b45f437bba9
ntalluri <nehatalluri@live.com>
2025-03-05 16:17:09 -0600

    updated test

--- test/parse-outputs/test_parse_outputs.py
+++ test/parse-outputs/test_parse_outputs.py
@@ -29,30 +29,29 @@ class TestParseOutputs:
             out_file = OUTDIR + f"{algo}-pathway.txt"
 
             runner.parse_output(algo, test_file, out_file)
-            print(algo)
             assert filecmp.cmp(OUTDIR + f"{algo}-pathway.txt", EXPDIR + f"{algo}-pathway-expected.txt", shallow=False)
 
-    # def test_empty_file(self):
-    #     for algo in algorithms:
-    #         test_file = INDIR + f"empty-raw-pathway.txt"
-    #         out_file = OUTDIR + f"{algo}-empty-pathway.txt"
+    def test_empty_file(self):
+        for algo in algorithms:
+            test_file = INDIR + f"empty-raw-pathway.txt"
+            out_file = OUTDIR + f"{algo}-empty-pathway.txt"
 
-    #         runner.parse_output(algo, test_file, out_file)
-    #         assert filecmp.cmp(OUTDIR + f"{algo}-empty-pathway.txt", EXPDIR + f"empty-pathway-expected.txt", shallow=False)
+            runner.parse_output(algo, test_file, out_file)
+            assert filecmp.cmp(OUTDIR + f"{algo}-empty-pathway.txt", EXPDIR + f"empty-pathway-expected.txt", shallow=False)
 
-    # def test_oi2_miss_insolution(self):
-    #     test_file = OI2_EDGE_CASES_INDIR + f"omicsintegrator2-miss-insolution-raw-pathway.txt"
-    #     out_file = OUTDIR + f"omicsintegrator2-miss-insolution-pathway.txt"
+    def test_oi2_miss_insolution(self):
+        test_file = OI2_EDGE_CASES_INDIR + f"omicsintegrator2-miss-insolution-raw-pathway.txt"
+        out_file = OUTDIR + f"omicsintegrator2-miss-insolution-pathway.txt"
 
-    #     runner.parse_output('omicsintegrator2', test_file, out_file)
-    #     assert filecmp.cmp(out_file, EXPDIR + f"empty-pathway-expected.txt", shallow=False)
+        runner.parse_output('omicsintegrator2', test_file, out_file)
+        assert filecmp.cmp(out_file, EXPDIR + f"empty-pathway-expected.txt", shallow=False)
 
-    # def test_oi2_wrong_order(self):
-    #     test_file = OI2_EDGE_CASES_INDIR + f"omicsintegrator2-wrong-order-raw-pathway.txt"
-    #     out_file = OUTDIR + f"omicsintegrator2-wrong-order-pathway.txt"
+    def test_oi2_wrong_order(self):
+        test_file = OI2_EDGE_CASES_INDIR + f"omicsintegrator2-wrong-order-raw-pathway.txt"
+        out_file = OUTDIR + f"omicsintegrator2-wrong-order-pathway.txt"
 
-    #     runner.parse_output('omicsintegrator2', test_file, out_file)
-    #     assert filecmp.cmp(out_file, EXPDIR + f"omicsintegrator2-pathway-expected.txt", shallow=False)
+        runner.parse_output('omicsintegrator2', test_file, out_file)
+        assert filecmp.cmp(out_file, EXPDIR + f"omicsintegrator2-pathway-expected.txt", shallow=False)
 
     def test_duplicate_edges(self):
         for algo in algorithms:
@@ -60,5 +59,4 @@ class TestParseOutputs:
             out_file = OUTDIR + f"{algo}-duplicate-pathway.txt"
 
             runner.parse_output(algo, test_file, out_file)
-            print(algo)
             assert filecmp.cmp(OUTDIR + f"{algo}-duplicate-pathway.txt", EXPDIR + f"{algo}-pathway-expected.txt", shallow=False)
