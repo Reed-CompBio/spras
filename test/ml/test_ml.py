added89cde6230aad6c9dfd79042b6e5fd14c16c
ntalluri <nehatalluri@live.com>
2025-03-24 13:22:34 -0500

    readd ml test cases

--- test/ml/test_ml.py
+++ test/ml/test_ml.py
@@ -20,15 +20,9 @@ class TestML:
         Path(OUT_DIR).mkdir(parents=True, exist_ok=True)
 
     def test_summarize_networks(self):
-        dataframe = ml.summarize_networks([INPUT_DIR + 'test-data-s1/s1.txt',
-                                           INPUT_DIR + 'test-data-s2/s2.txt',
-                                           INPUT_DIR + 'test-data-s3/s3.txt',
-                                           INPUT_DIR + 'test-data-longName/longName.txt',
-                                           INPUT_DIR + 'test-data-longName2/longName2.txt',
-                                           INPUT_DIR + 'test-data-empty/empty.txt',
-                                           INPUT_DIR + 'test-data-spaces/spaces.txt',
-                                           INPUT_DIR + 'test-data-mixed-direction/mixed-direction.txt',
-                                           INPUT_DIR + 'test-data-repeat-edges-directed/repeat-edges-directed.txt'])
+        dataframe = ml.summarize_networks([INPUT_DIR + 'test-data-s1/s1.txt', INPUT_DIR + 'test-data-s2/s2.txt', INPUT_DIR + 'test-data-s3/s3.txt',
+                                           INPUT_DIR + 'test-data-longName/longName.txt', INPUT_DIR + 'test-data-longName2/longName2.txt',
+                                           INPUT_DIR + 'test-data-empty/empty.txt', INPUT_DIR + 'test-data-spaces/spaces.txt', INPUT_DIR + 'test-data-mixed-direction/mixed-direction.txt'])
         dataframe.to_csv(OUT_DIR + 'dataframe.csv')
         assert filecmp.cmp(OUT_DIR + 'dataframe.csv', EXPECT_DIR + 'expected-dataframe.csv', shallow=False)
 
@@ -50,7 +44,7 @@ class TestML:
 
     def test_empty(self):
         dataframe = ml.summarize_networks([INPUT_DIR + 'test-data-empty/empty.txt'])
-        with pytest.raises(ValueError):  # raises error if empty dataframe is used for post-processing
+        with pytest.raises(ValueError):  # raises error if empty dataframe is used for post processing
             ml.pca(dataframe, OUT_DIR + 'pca-empty.png', OUT_DIR + 'pca-empty-variance.txt',
                OUT_DIR + 'pca-empty-coordinates.tsv')
         with pytest.raises(ValueError):
@@ -83,14 +77,15 @@ class TestML:
         dataframe = ml.summarize_networks([INPUT_DIR + 'test-data-s1/s1.txt', INPUT_DIR + 'test-data-s2/s2.txt', INPUT_DIR + 'test-data-s3/s3.txt'])
         expected = pd.read_table(EXPECT_DIR + 'expected-pca-coordinates.tsv')
         expected = expected.round(5)
+        expected.sort_values(by='datapoint_labels', ignore_index=True, inplace=True)
+
         for _ in range(5):
             dataframe_shuffled = dataframe.sample(frac=1, axis=1)  # permute the columns
             ml.pca(dataframe_shuffled, OUT_DIR + 'pca-shuffled-columns.png', OUT_DIR + 'pca-shuffled-columns-variance.txt',
                 OUT_DIR + 'pca-shuffled-columns-coordinates.tsv')
             coord = pd.read_table(OUT_DIR + 'pca-shuffled-columns-coordinates.tsv')
             coord = coord.round(5)  # round values to 5 digits to account for numeric differences across machines
-            coord.sort_values(by='algorithm', ignore_index=True, inplace=True)
-
+            coord.sort_values(by='datapoint_labels', ignore_index=True, inplace=True)
             assert coord.equals(expected)
 
         for _ in range(5):
@@ -99,7 +94,7 @@ class TestML:
                     OUT_DIR + 'pca-shuffled-rows-coordinates.tsv')
             coord = pd.read_table(OUT_DIR + 'pca-shuffled-rows-coordinates.tsv')
             coord = coord.round(5)  # round values to 5 digits to account for numeric differences across machines
-            coord.sort_values(by='algorithm', ignore_index=True, inplace=True)
+            coord.sort_values(by='datapoint_labels', ignore_index=True, inplace=True)
 
             assert coord.equals(expected)
 
