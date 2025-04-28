fb6b1ae6e1e34380f0119b4f2fab61445d4d34e2
Carol <cgsze2@gmail.com>
2025-04-28 16:01:14 -0500

    output/ directory issues (to be continued)

--- test/analysis/test_summary.py
+++ test/analysis/test_summary.py
@@ -14,11 +14,16 @@ from spras.dataset import Dataset
 # - 'NODEID' is required as the first column label in the node table
 # - file_paths must be an iterable, even if a single file path is passed
 
+INPUT_DIR = 'test/analysis/input/'
+OUT_DIR = 'test/analysis/output/'
+EXPECT_DIR = 'test/analysis/expected_output/'
+
 class TestSummary:
-    # hard code file paths to run from root directory
-    root = Path(__file__).resolve().parents[2]
-    inp = root / "test/analysis/input"
-    exp_out = root / "test/analysis/expected_output"
+    def setup_class(cls):
+        """
+        Create the expected output directory
+        """
+        Path(OUT_DIR).mkdir(parents=True, exist_ok=True)
 
     # Test data from example workflow:
     def test_example_networks(self):
@@ -30,14 +35,14 @@ class TestSummary:
                        }
         example_dataset = Dataset(example_dict)
         example_node_table = example_dataset.node_table
-        config.init_from_file(self.inp / "config.yaml")
+        config.init_from_file(INPUT_DIR + "config.yaml")
         algorithm_params = config.config.algorithm_params
         list(algorithm_params)
         algorithms_with_params = [f'{algorithm}-params-{params_hash}' for algorithm, param_combos in algorithm_params.items() for params_hash in param_combos.keys()]
 
-        example_network_files = (self.inp /"example").glob("*.txt") # must be path to use .glob()
+        example_network_files = (INPUT_DIR + "example").glob("*.txt") # must be path to use .glob()
         #example_node_table = pd.read_csv(Path("test/analysis/input/example_node_table.txt"), sep = "\t") #old
-        example_output = pd.read_csv((self.exp_out / "test_example_summary.txt"), sep = "\t")
+        example_output = pd.read_csv((EXPECT_DIR + "test_example_summary.txt"), sep = "\t")
         example_output["Name"] = example_output["Name"].map(convert_path)
         summarize_example = summarize_networks(example_network_files, example_node_table, algorithm_params, algorithms_with_params)
         assert summarize_example.equals(example_output)
@@ -53,15 +58,15 @@ class TestSummary:
 
         egfr_dataset = Dataset(egfr_dict)
         egfr_node_table = egfr_dataset.node_table
-        config.init_from_file(self.inp / "egfr.yaml")
+        config.init_from_file(INPUT_DIR + "egfr.yaml")
         algorithm_params = config.config.algorithm_params
         list(algorithm_params)
         algorithms_with_params = [f'{algorithm}-params-{params_hash}' for algorithm, param_combos in
                                   algorithm_params.items() for params_hash in param_combos.keys()]
 
-        egfr_network_files = (self.inp / "egfr").glob("*.txt")  # must be path to use .glob()
+        egfr_network_files = (INPUT_DIR + "egfr").glob("*.txt")  # must be path to use .glob()
         # egfr_node_table = pd.read_csv(Path("test/analysis/input/egfr_node_table.txt"), sep = "\t") #old
-        egfr_output = pd.read_csv((self.exp_out / "test_egfr_summary.txt"), sep="\t")
+        egfr_output = pd.read_csv((EXPECT_DIR + "test_egfr_summary.txt"), sep="\t")
         egfr_output["Name"] = egfr_output["Name"].map(convert_path)
         summarize_egfr = summarize_networks(egfr_network_files, egfr_node_table, algorithm_params,
                                                algorithms_with_params)
@@ -80,7 +85,7 @@ class TestSummary:
 
         # node_table contents are not generated consistently in the same order,
         # so we will check that the contents are the same, but row order doesn't matter
-        expected_node_table = pd.read_csv((self.exp_out / "expected_node_table.txt"), sep="\t")
+        expected_node_table = pd.read_csv((EXPECT_DIR + "expected_node_table.txt"), sep="\t")
 
         # ignore 'NODEID' column because this changes each time upon new generation
         cols_to_compare = [col for col in example_node_table.columns if col != "NODEID"]
