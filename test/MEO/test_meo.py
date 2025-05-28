971606d5d04512de50f5324d9b8109739baddc37
Tristan F.-R. <pub.tristanf@gmail.com>
2025-05-28 11:03:29 -0700

    test(MEO): empty files
    Addresses #233

--- test/MEO/test_meo.py
+++ test/MEO/test_meo.py
@@ -8,8 +8,8 @@ from spras.meo import MEO, write_properties
 
 config.init_from_file("config/config.yaml")
 
-TEST_DIR = 'test/MEO/'
-OUT_FILE = TEST_DIR + 'output/edges.txt'
+TEST_DIR = Path('test', 'MEO')
+OUT_FILE = TEST_DIR / 'output' / 'edges.txt'
 
 
 class TestMaximumEdgeOrientation:
@@ -17,54 +17,65 @@ class TestMaximumEdgeOrientation:
     Run Maximum Edge Orientation in the Docker image
     """
     def test_meo_required(self):
-        out_path = Path(OUT_FILE)
-        out_path.unlink(missing_ok=True)
+        OUT_FILE.unlink(missing_ok=True)
         # Only include required arguments
-        MEO.run(edges=TEST_DIR + 'input/meo-edges.txt',
-                sources=TEST_DIR + 'input/meo-sources.txt',
-                targets=TEST_DIR + 'input/meo-targets.txt',
+        MEO.run(edges=TEST_DIR / 'input' / 'meo-edges.txt',
+                sources=TEST_DIR / 'input' / 'meo-sources.txt',
+                targets=TEST_DIR / 'input' / 'meo-targets.txt',
                 output_file=OUT_FILE)
-        assert out_path.exists()
+        assert OUT_FILE.exists()
 
     def test_meo_all_optional(self):
-        out_path = Path(OUT_FILE)
-        out_path.unlink(missing_ok=True)
+        OUT_FILE.unlink(missing_ok=True)
         # Include all optional arguments
-        MEO.run(edges=TEST_DIR + 'input/meo-edges.txt',
-                sources=TEST_DIR + 'input/meo-sources.txt',
-                targets=TEST_DIR + 'input/meo-targets.txt',
+        MEO.run(edges=TEST_DIR / 'input' / 'meo-edges.txt',
+                sources=TEST_DIR / 'input' / 'meo-sources.txt',
+                targets=TEST_DIR / 'input' / 'meo-targets.txt',
                 output_file=OUT_FILE,
                 max_path_length=3,
                 local_search='No',
                 rand_restarts=10)
-        assert out_path.exists()
+        assert OUT_FILE.exists()
+
+    def test_meo_empty(self):
+        OUT_FILE.unlink(missing_ok=True)
+
+        MEO.run(edges=TEST_DIR / 'input' / 'empty-edges.txt',
+                sources=TEST_DIR / 'input' / 'empty-sources.txt',
+                targets=TEST_DIR / 'input' / 'empty-targets.txt',
+                output_file=OUT_FILE)
+        assert OUT_FILE.exists()
+
+        MEO.parse_output(
+            raw_pathway_file=OUT_FILE,
+            standardized_pathway_file=TEST_DIR / 'output' / 'edges-standardized.txt'
+        )
 
     def test_meo_missing(self):
         # Test the expected error is raised when required arguments are missing
         with pytest.raises(ValueError):
             # No edges
-            MEO.run(sources=TEST_DIR + 'input/meo-sources.txt',
-                    targets=TEST_DIR + 'input/meo-targets.txt',
+            MEO.run(sources=TEST_DIR / 'input' / 'meo-sources.txt',
+                    targets=TEST_DIR / 'input' / 'meo-targets.txt',
                     output_file=OUT_FILE)
 
         with pytest.raises(ValueError):
             # No path_output
             write_properties(filename=Path('.'),
-                             edges=TEST_DIR + 'input/meo-edges.txt',
-                             sources=TEST_DIR + 'input/meo-sources.txt',
-                             targets=TEST_DIR + 'input/meo-targets.txt',
+                             edges=TEST_DIR / 'input' / 'meo-edges.txt',
+                             sources=TEST_DIR / 'input' / 'meo-sources.txt',
+                             targets=TEST_DIR / 'input' / 'meo-targets.txt',
                              edge_output=OUT_FILE)
 
     # Only run Singularity test if the binary is available on the system
     # spython is only available on Unix, but do not explicitly skip non-Unix platforms
     @pytest.mark.skipif(not shutil.which('singularity'), reason='Singularity not found on system')
     def test_meo_singularity(self):
-        out_path = Path(OUT_FILE)
-        out_path.unlink(missing_ok=True)
+        OUT_FILE.unlink(missing_ok=True)
         # Only include required arguments and run with Singularity
-        MEO.run(edges=TEST_DIR + 'input/meo-edges.txt',
-                sources=TEST_DIR + 'input/meo-sources.txt',
-                targets=TEST_DIR + 'input/meo-targets.txt',
+        MEO.run(edges=TEST_DIR / 'input' / 'meo-edges.txt',
+                sources=TEST_DIR / 'input' / 'meo-sources.txt',
+                targets=TEST_DIR / 'input' / 'meo-targets.txt',
                 output_file=OUT_FILE,
                 container_framework="singularity")
-        assert out_path.exists()
+        assert OUT_FILE.exists()
