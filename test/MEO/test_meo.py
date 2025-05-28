15c585e5526d952f8c65c0f2b8eb1bc887bfd85f
Tristan F.-R. <pub.tristanf@gmail.com>
2025-05-28 11:40:40 -0700

    test: meo_present_headers

--- test/MEO/test_meo.py
+++ test/MEO/test_meo.py
@@ -1,4 +1,3 @@
-import filecmp
 import shutil
 from pathlib import Path
 
@@ -9,10 +8,8 @@ from spras.meo import MEO, write_properties
 
 config.init_from_file("config/config.yaml")
 
-TEST_DIR = Path('test', 'MEO')
-OUT_FILE = TEST_DIR / 'output' / 'edges.txt'
-EXPECTED_FILE = TEST_DIR / 'expected' / 'edges-standardized.txt'
-OUT_FILE_PROCESSED = TEST_DIR / 'output' / 'edges-standardized.txt'
+TEST_DIR = 'test/MEO/'
+OUT_FILE = TEST_DIR + 'output/edges.txt'
 
 
 class TestMaximumEdgeOrientation:
@@ -20,67 +17,54 @@ class TestMaximumEdgeOrientation:
     Run Maximum Edge Orientation in the Docker image
     """
     def test_meo_required(self):
-        OUT_FILE.unlink(missing_ok=True)
+        out_path = Path(OUT_FILE)
+        out_path.unlink(missing_ok=True)
         # Only include required arguments
-        MEO.run(edges=TEST_DIR / 'input' / 'meo-edges.txt',
-                sources=TEST_DIR / 'input' / 'meo-sources.txt',
-                targets=TEST_DIR / 'input' / 'meo-targets.txt',
+        MEO.run(edges=TEST_DIR + 'input/meo-edges.txt',
+                sources=TEST_DIR + 'input/meo-sources.txt',
+                targets=TEST_DIR + 'input/meo-targets.txt',
                 output_file=OUT_FILE)
-        assert OUT_FILE.exists()
+        assert out_path.exists()
 
     def test_meo_all_optional(self):
-        OUT_FILE.unlink(missing_ok=True)
+        out_path = Path(OUT_FILE)
+        out_path.unlink(missing_ok=True)
         # Include all optional arguments
-        MEO.run(edges=TEST_DIR / 'input' / 'meo-edges.txt',
-                sources=TEST_DIR / 'input' / 'meo-sources.txt',
-                targets=TEST_DIR / 'input' / 'meo-targets.txt',
+        MEO.run(edges=TEST_DIR + 'input/meo-edges.txt',
+                sources=TEST_DIR + 'input/meo-sources.txt',
+                targets=TEST_DIR + 'input/meo-targets.txt',
                 output_file=OUT_FILE,
                 max_path_length=3,
                 local_search='No',
                 rand_restarts=10)
-        assert OUT_FILE.exists()
-
-    def test_meo_empty(self):
-        OUT_FILE.unlink(missing_ok=True)
-
-        MEO.run(edges=TEST_DIR / 'input' / 'empty-edges.txt',
-                sources=TEST_DIR / 'input' / 'empty-sources.txt',
-                targets=TEST_DIR / 'input' / 'empty-targets.txt',
-                output_file=OUT_FILE)
-        assert OUT_FILE.exists()
-
-        MEO.parse_output(
-            raw_pathway_file=OUT_FILE,
-            standardized_pathway_file=OUT_FILE_PROCESSED
-        )
-
-        assert filecmp.cmp(EXPECTED_FILE, OUT_FILE_PROCESSED, shallow=False)
+        assert out_path.exists()
 
     def test_meo_missing(self):
         # Test the expected error is raised when required arguments are missing
         with pytest.raises(ValueError):
             # No edges
-            MEO.run(sources=TEST_DIR / 'input' / 'meo-sources.txt',
-                    targets=TEST_DIR / 'input' / 'meo-targets.txt',
+            MEO.run(sources=TEST_DIR + 'input/meo-sources.txt',
+                    targets=TEST_DIR + 'input/meo-targets.txt',
                     output_file=OUT_FILE)
 
         with pytest.raises(ValueError):
             # No path_output
             write_properties(filename=Path('.'),
-                             edges=TEST_DIR / 'input' / 'meo-edges.txt',
-                             sources=TEST_DIR / 'input' / 'meo-sources.txt',
-                             targets=TEST_DIR / 'input' / 'meo-targets.txt',
+                             edges=TEST_DIR + 'input/meo-edges.txt',
+                             sources=TEST_DIR + 'input/meo-sources.txt',
+                             targets=TEST_DIR + 'input/meo-targets.txt',
                              edge_output=OUT_FILE)
 
     # Only run Singularity test if the binary is available on the system
     # spython is only available on Unix, but do not explicitly skip non-Unix platforms
     @pytest.mark.skipif(not shutil.which('singularity'), reason='Singularity not found on system')
     def test_meo_singularity(self):
-        OUT_FILE.unlink(missing_ok=True)
+        out_path = Path(OUT_FILE)
+        out_path.unlink(missing_ok=True)
         # Only include required arguments and run with Singularity
-        MEO.run(edges=TEST_DIR / 'input' / 'meo-edges.txt',
-                sources=TEST_DIR / 'input' / 'meo-sources.txt',
-                targets=TEST_DIR / 'input' / 'meo-targets.txt',
+        MEO.run(edges=TEST_DIR + 'input/meo-edges.txt',
+                sources=TEST_DIR + 'input/meo-sources.txt',
+                targets=TEST_DIR + 'input/meo-targets.txt',
                 output_file=OUT_FILE,
                 container_framework="singularity")
-        assert OUT_FILE.exists()
+        assert out_path.exists()
--- test/parse-outputs/test_parse_outputs.py
+++ test/parse-outputs/test_parse_outputs.py
@@ -1,13 +1,14 @@
 import filecmp
 from pathlib import Path
 
+import pytest
 from spras import runner
 
-INDIR = "test/parse-outputs/input/"
-OUTDIR = "test/parse-outputs/output/"
-EXPDIR = "test/parse-outputs/expected/"
-OI2_EDGE_CASES_INDIR = 'test/parse-outputs/input/omicsintegrator-edge-cases/'
-DUPLICATE_EDGES_DIR = 'test/parse-outputs/input/duplicate-edges/'
+INDIR = Path("test", "parse-outputs", "input")
+OUTDIR = Path("test", "parse-outputs", "output")
+EXPDIR = Path("test", "parse-outputs", "expected")
+OI2_EDGE_CASES_INDIR = Path("test", "parse-outputs", "input", "omicsintegrator-edge-cases")
+DUPLICATE_EDGES_DIR = Path("test", "parse-outputs", "input", "duplicate-edges")
 # DOMINO input is the concatenated module_0.html and module_1.html file from
 # the DOMINO output of the network dip.sif and the nodes tnfa_active_genes_file.txt
 # from https://github.com/Shamir-Lab/DOMINO/tree/master/examples
@@ -21,42 +22,49 @@ class TestParseOutputs:
         """
         Create the expected output directory
         """
-        Path(OUTDIR).mkdir(parents=True, exist_ok=True)
+        OUTDIR.mkdir(parents=True, exist_ok=True)
 
     def test_parse_outputs(self):
         for algo in algorithms:
-            test_file = INDIR + f"{algo}-raw-pathway.txt"
-            out_file = OUTDIR + f"{algo}-pathway.txt"
+            test_file = INDIR / f"{algo}-raw-pathway.txt"
+            out_file = OUTDIR / f"{algo}-pathway.txt"
 
             runner.parse_output(algo, test_file, out_file)
-            assert filecmp.cmp(OUTDIR + f"{algo}-pathway.txt", EXPDIR + f"{algo}-pathway-expected.txt", shallow=False)
+            assert filecmp.cmp(OUTDIR / f"{algo}-pathway.txt", EXPDIR / f"{algo}-pathway-expected.txt", shallow=False)
 
     def test_empty_file(self):
         for algo in algorithms:
-            test_file = INDIR + f"empty-raw-pathway.txt"
-            out_file = OUTDIR + f"{algo}-empty-pathway.txt"
+            test_file = INDIR / f"empty-raw-pathway.txt"
+            out_file = OUTDIR / f"{algo}-empty-pathway.txt"
 
             runner.parse_output(algo, test_file, out_file)
-            assert filecmp.cmp(OUTDIR + f"{algo}-empty-pathway.txt", EXPDIR + f"empty-pathway-expected.txt", shallow=False)
+            assert filecmp.cmp(OUTDIR / f"{algo}-empty-pathway.txt", EXPDIR / f"empty-pathway-expected.txt", shallow=False)
 
     def test_oi2_miss_insolution(self):
-        test_file = OI2_EDGE_CASES_INDIR + f"omicsintegrator2-miss-insolution-raw-pathway.txt"
-        out_file = OUTDIR + f"omicsintegrator2-miss-insolution-pathway.txt"
+        test_file = OI2_EDGE_CASES_INDIR / f"omicsintegrator2-miss-insolution-raw-pathway.txt"
+        out_file = OUTDIR / f"omicsintegrator2-miss-insolution-pathway.txt"
 
         runner.parse_output('omicsintegrator2', test_file, out_file)
-        assert filecmp.cmp(out_file, EXPDIR + f"empty-pathway-expected.txt", shallow=False)
+        assert filecmp.cmp(out_file, EXPDIR / f"empty-pathway-expected.txt", shallow=False)
 
     def test_oi2_wrong_order(self):
-        test_file = OI2_EDGE_CASES_INDIR + f"omicsintegrator2-wrong-order-raw-pathway.txt"
-        out_file = OUTDIR + f"omicsintegrator2-wrong-order-pathway.txt"
+        test_file = OI2_EDGE_CASES_INDIR / f"omicsintegrator2-wrong-order-raw-pathway.txt"
+        out_file = OUTDIR / f"omicsintegrator2-wrong-order-pathway.txt"
 
         runner.parse_output('omicsintegrator2', test_file, out_file)
-        assert filecmp.cmp(out_file, EXPDIR + f"omicsintegrator2-pathway-expected.txt", shallow=False)
+        assert filecmp.cmp(out_file, EXPDIR / f"omicsintegrator2-pathway-expected.txt", shallow=False)
 
     def test_duplicate_edges(self):
         for algo in algorithms:
-            test_file = DUPLICATE_EDGES_DIR + f"{algo}-raw-pathway.txt"
-            out_file = OUTDIR + f"{algo}-duplicate-pathway.txt"
+            test_file = DUPLICATE_EDGES_DIR / f"{algo}-raw-pathway.txt"
+            out_file = OUTDIR / f"{algo}-duplicate-pathway.txt"
 
             runner.parse_output(algo, test_file, out_file)
-            assert filecmp.cmp(OUTDIR + f"{algo}-duplicate-pathway.txt", EXPDIR + f"{algo}-pathway-expected.txt", shallow=False)
+            assert filecmp.cmp(OUTDIR / f"{algo}-duplicate-pathway.txt", EXPDIR / f"{algo}-pathway-expected.txt", shallow=False)
+    
+    def test_meo_present_headers(self):
+        test_file = INDIR / f"meo-present-headers.txt"
+        out_file = OUTDIR / f"meo-present-headers-output.txt"
+
+        runner.parse_output('meo', test_file, out_file)
+        assert filecmp.cmp(OUTDIR / f"meo-present-headers-output.txt", EXPDIR / f"meo-present-headers-expected.txt", shallow=False)
