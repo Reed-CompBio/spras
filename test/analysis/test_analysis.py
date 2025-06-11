23befd7c6bd788559537a85a0d4a796a76facdf2
Tristan F.-R. <pub.tristanf@gmail.com>
2025-06-11 11:11:52 -0700

    refactor: drop graphspace
    GraphSpace is currently unmaintained

--- test/analysis/test_analysis.py
+++ test/analysis/test_analysis.py
@@ -1,16 +1,12 @@
 from pathlib import Path
 
-import spras.analysis.graphspace as graphspace
+# TOOD: this test file is effectively empty after the removal of GraphSpace
 
 IN_DIR = 'test/analysis/input/'
 OUT_DIR = 'test/analysis/output/'
 
 
 class TestAnalysis:
-    """
-    Test GraphSpace
-    """
-
     @classmethod
     def setup_class(cls):
         """
@@ -34,15 +30,3 @@ class TestAnalysis:
     # # test a ranked file interpreted as a directed network (e.g. uses nx.DiGraph).
     # def test_summary_directed(self):
     #     summary.run(TEST_DIR + 'input/standardized-ranked.txt', TEST_DIR + 'output/standardized-ranked-directed-out.txt')
-
-    # test GraphSpace json output on an undirected graph.
-    def test_graphspace_ranked(self):
-        graphspace.write_json(IN_DIR + 'standardized-ranked.txt',
-                              OUT_DIR + 'standardized-ranked-undirected-gs.json',
-                              OUT_DIR + 'standardized-ranked-undirected-gs-style.json')
-
-    # test GraphSpace json output on a directed graph.
-    def test_graphspace_ranked_directed(self):
-        graphspace.write_json(IN_DIR + 'standardized-ranked.txt',
-                              OUT_DIR + 'standardized-ranked-directed-gs.json',
-                              OUT_DIR + 'standardized-ranked-directed-gs-style.json')
