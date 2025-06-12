e214d70ab347d076558a26da034e9bfefe96a66c
Tristan F.-R. <pub.tristanf@gmail.com>
2025-06-12 19:00:07 +0000

    fix: re-add indexer for st mask

--- spras/pathlinker.py
+++ spras/pathlinker.py
@@ -48,7 +48,7 @@ class PathLinker(PRM):
             warnings.warn(warn_msg, stacklevel=1)
 
         # Create nodetype file
-        input_df = sources_targets[Dataset.NODE_ID].copy()
+        input_df = sources_targets[[Dataset.NODE_ID]].copy()
         input_df.columns = ["#Node"]
         input_df.loc[sources_targets["sources"] == True,"Node type"]="source"
         input_df.loc[sources_targets["targets"] == True,"Node type"]="target"
