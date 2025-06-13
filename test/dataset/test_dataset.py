8e4016b57dd57e67e20ed65da9677edbb1ffe148
Tristan F.-R. <pub.tristanf@gmail.com>
2025-06-13 09:30:18 -0700

    test: dataset

---
+++ test/dataset/test_dataset.py
@@ -0,0 +1,28 @@
+import pandas
+from pathlib import Path
+import pytest
+
+from spras.dataset import Dataset
+
+FIXTURES_PATH = Path('test', 'dataset', 'fixtures')
+
+class TestDataset:
+    def test_not_allow_no_cols(self):
+        with pytest.raises(pandas.errors.EmptyDataError):
+            Dataset({
+                'label': 'empty',
+                'edge_files': ['network.txt'],
+                'node_files': ['scores.txt', 'nodes.txt'],
+                'other_files': [],
+                'data_dir': FIXTURES_PATH / 'empty'
+            })
+    
+    def test_not_allow_edge_weights_oor(self):
+        with pytest.raises(ValueError):
+            Dataset({
+                'label': 'empty',
+                'edge_files': ['network.txt'],
+                'node_files': ['node-prizes.txt', 'sources.txt', 'targets.txt'],
+                'other_files': [],
+                'data_dir': FIXTURES_PATH / 'in-range'
+            })
