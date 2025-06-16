397f718662dfa6bc9f7cc1fe86b65c5b4c0ee22e
Tristan F.-R. <pub.tristanf@gmail.com>
2025-06-16 13:32:02 -0700

    style: fmt

--- test/ST_RWR/test_STRWR.py
+++ test/ST_RWR/test_STRWR.py
@@ -50,7 +50,7 @@ class TestSTRWR:
                        targets=Path(TEST_DIR, 'input','strwr-targets.txt'),
                        alpha=0.85,
                        output_file=OUT_FILE)
-    
+
     # Only run Singularity test if the binary is available on the system
     # spython is only available on Unix, but do not explicitly skip non-Unix platforms
     @pytest.mark.skipif(not shutil.which('singularity'), reason='Singularity not found on system')
