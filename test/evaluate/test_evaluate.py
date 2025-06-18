c3fe362d16371421a78d25e4480d92b6c6c77f9b
ntalluri <nehatalluri@live.com>
2025-06-18 14:23:55 -0500

    updated test cases

--- test/evaluate/test_evaluate.py
+++ test/evaluate/test_evaluate.py
@@ -20,24 +20,31 @@ class TestEvaluate:
         Path(OUT_DIR).mkdir(parents=True, exist_ok=True)
 
     def test_node_ensemble(self):
+        out_path_file = Path(OUT_DIR+'node-ensemble.csv')
+        out_path_file.unlink(missing_ok=True)
         ensemble_network = [INPUT_DIR + 'ensemble-network.tsv']
-        node_ensemble_dict = Evaluation.edge_frequency_node_ensemble(GS_NODE_TABLE, ensemble_network)
-        node_ensemble_dict['ensemble'].to_csv(OUT_DIR + 'node-ensemble.csv', sep="\t", index=False)
-        assert filecmp.cmp(OUT_DIR + 'node-ensemble.csv', EXPECT_DIR + 'expected-node-ensemble.csv', shallow=False)
+        input_network = INPUT_DIR + 'data.pickle'
+        node_ensemble_dict = Evaluation.edge_frequency_node_ensemble(GS_NODE_TABLE, ensemble_network, input_network)
+        node_ensemble_dict['ensemble'].to_csv(out_path_file, sep="\t", index=False)
+        assert filecmp.cmp(out_path_file, EXPECT_DIR + 'expected-node-ensemble.csv', shallow=False)
 
     def test_empty_node_ensemble(self):
+        out_path_file = Path(OUT_DIR+ 'empty-node-ensemble.csv')
+        out_path_file.unlink(missing_ok=True)
         empty_ensemble_network = [INPUT_DIR + 'empty-ensemble-network.tsv']
-        node_ensemble_dict = Evaluation.edge_frequency_node_ensemble(GS_NODE_TABLE, empty_ensemble_network)
-        node_ensemble_dict['empty'].to_csv(OUT_DIR + 'empty-node-ensemble.csv', sep="\t", index=False)
-        assert filecmp.cmp(OUT_DIR + 'empty-node-ensemble.csv', EXPECT_DIR + 'expected-node-ensemble-empty.csv', shallow=False)
+        input_network = INPUT_DIR + 'data.pickle'
+        node_ensemble_dict = Evaluation.edge_frequency_node_ensemble(GS_NODE_TABLE, empty_ensemble_network, input_network)
+        node_ensemble_dict['empty'].to_csv(out_path_file, sep="\t", index=False)
+        assert filecmp.cmp(out_path_file, EXPECT_DIR + 'expected-empty-node-ensemble.csv', shallow=False)
 
     def test_multiple_node_ensemble(self):
         ensemble_networks = [INPUT_DIR + 'ensemble-network.tsv', INPUT_DIR + 'empty-ensemble-network.tsv']
-        node_ensemble_dict = Evaluation.edge_frequency_node_ensemble(GS_NODE_TABLE, ensemble_networks)
+        input_network = INPUT_DIR + 'data.pickle'
+        node_ensemble_dict = Evaluation.edge_frequency_node_ensemble(GS_NODE_TABLE, ensemble_networks, input_network)
         node_ensemble_dict['ensemble'].to_csv(OUT_DIR + 'node-ensemble.csv', sep="\t", index=False)
         assert filecmp.cmp(OUT_DIR + 'node-ensemble.csv', EXPECT_DIR + 'expected-node-ensemble.csv', shallow=False)
         node_ensemble_dict['empty'].to_csv(OUT_DIR + 'empty-node-ensemble.csv', sep="\t", index=False)
-        assert filecmp.cmp(OUT_DIR + 'empty-node-ensemble.csv', EXPECT_DIR + 'expected-node-ensemble-empty.csv', shallow=False)
+        assert filecmp.cmp(OUT_DIR + 'empty-node-ensemble.csv', EXPECT_DIR + 'expected-empty-node-ensemble.csv', shallow=False)
 
     def test_precision_recall_curve_ensemble_nodes(self):
         out_path_png = Path(OUT_DIR+"pr-curve-ensemble-nodes.png")
@@ -72,15 +79,3 @@ class TestEvaluate:
         Evaluation.precision_recall_curve_node_ensemble(node_ensembles_dict, GS_NODE_TABLE, out_path_png, out_path_file)
         assert out_path_png.exists()
         assert filecmp.cmp(out_path_file, EXPECT_DIR + 'expected-pr-curve-multiple-ensemble-nodes.txt', shallow=False)
-
-    def test_precision_recall_curve_multiple_empty_ensemble_nodes(self):
-        out_path_png = Path(OUT_DIR+"pr-curve-multiple-empty-ensemble-nodes.png")
-        out_path_png.unlink(missing_ok=True)
-        out_path_file = Path(OUT_DIR+"pr-curve-multiple-empty-ensemble-nodes.txt")
-        out_path_file.unlink(missing_ok=True)
-        empty_ensemble_file = pd.read_csv(INPUT_DIR + 'node-ensemble-empty.csv', sep="\t", header=0)
-        node_ensembles_dict = {'ensemble1': empty_ensemble_file, 'ensemble2': empty_ensemble_file, 'ensemble3': empty_ensemble_file}
-        Evaluation.precision_recall_curve_node_ensemble(node_ensembles_dict, GS_NODE_TABLE, out_path_png, out_path_file)
-        assert out_path_png.exists()
-        assert filecmp.cmp(out_path_file, EXPECT_DIR + 'expected-pr-curve-multiple-empty-ensemble-nodes.txt', shallow=False)
-
