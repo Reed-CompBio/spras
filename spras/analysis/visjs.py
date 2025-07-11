75fb7a7224ffb6b5302b6cd011e616e578f17e83
Tristan F.-R. <pub.tristanf@gmail.com>
2025-07-11 10:47:02 -0700

    feat: actually use the visjs.html file

--- spras/analysis/visjs.py
+++ spras/analysis/visjs.py
@@ -2,9 +2,18 @@
 # which doesn't support mixed graphs.
 import pandas as pd
 from pathlib import Path
+import os
 
+# https://stackoverflow.com/a/5137509/7589775
+# The file we want, visjs.html, is also included in MANIFEST.in
+dir_path = os.path.dirname(os.path.realpath(__file__))
+visjs_html = Path(dir_path, "visjs.html").read_text()
 
-def visualize(network: pd.DataFrame, output: str):
+def visualize(network: pd.DataFrame) -> str:
+    """
+    Visualizes an output interactome
+    as a visjs .html file.
+    """
     current_id = 0
     node_ids: dict[str, int] = dict()
 
@@ -26,4 +35,10 @@ def visualize(network: pd.DataFrame, output: str):
             nodes_repr.append(f"{{id: {current_id}, label: '{target_node}'}}")
             current_id += 1
         
-        edges_repr.append(f"{{from: {node_ids[source_node]}, to: {node_ids[target_node]}}},")
+        edges_repr.append(f"{{from: {node_ids[source_node]}, to: {node_ids[target_node]}}}")
+    
+    output_html = visjs_html
+    output_html = output_html.replace("// {{nodes}}", ','.join(nodes_repr))
+    output_html = output_html.replace("// {{edges}}", ','.join(edges_repr))
+
+    return output_html
