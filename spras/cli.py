81f2285c98feb8422ae67327ed682de1f73c6632
Tristan F.-R. <pub.tristanf@gmail.com>
2025-07-15 23:35:20 +0000

    feat: cli

---
+++ spras/cli.py
@@ -0,0 +1,19 @@
+import itertools
+import os
+from pathlib import Path
+import subprocess
+import sys
+
+# https://stackoverflow.com/a/5137509/7589775
+# The file we want, visjs.html, is also included in MANIFEST.in
+dir_path = os.path.dirname(os.path.realpath(__file__))
+snakefile_path = Path(dir_path, "..", "Snakefile")
+
+def run():
+    subprocess.run(list(itertools.chain(
+        ["snakemake", "-s", snakefile_path],
+        sys.argv[1:]
+    )))
+
+if __name__ == '__main__':
+    run()
