9442b6496251823456670092f12b404dba19c76c
Tristan F.-R. <pub.tristanf@gmail.com>
2025-07-15 17:13:45 +0000

    chore: drop play

--- util/play.py
+++
@@ -1,5 +0,0 @@
-import ast
-value_ast = ast.parse("np.range.test(1, 2, 3)", mode='eval')
-# print(ast.dump(value_ast.body, indent=2))
-assert isinstance(value_ast.body, ast.Call)
-print([ast.literal_eval(arg) for arg in value_ast.body.args])
