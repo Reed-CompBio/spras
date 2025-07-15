398350e68a3f1776e829c5f7f4823560cd73f7b8
Tristan F. <LeoDog896@hotmail.com>
2025-07-15 08:22:58 -0700

    feat: more algs schema handling

--- spras/config/algorithms.py
+++ spras/config/algorithms.py
@@ -3,12 +3,39 @@ Dynamic construction of algoithm parameters with runtime type information for
 parameter combinations. This has been isolated from schema.py as it is not declarative,
 and rather mainly contains validators and lower-level pydantic code.
 """
-from typing import Any, cast, Union, Literal
+from typing import Annotated, Any, Callable, cast, Union, Literal
 
 from spras.runner import algorithms
-from pydantic import BaseModel, create_model
+from pydantic import BaseModel, BeforeValidator, create_model
 
 __all__ = ['AlgorithmUnion']
+
+def is_numpy_friendly(type: type[Any] | None) -> bool:
+    """
+    Whether the passed in type can have any numpy helpers.
+    This is mainly used to provide hints in the JSON schema.
+    """
+    return type in (int, float)
+
+def python_evalish_coerce(type: type[Any] | None) -> Callable[[Any], Any]:
+    """
+    Allows for using numpy and python calls
+    """
+    
+    def numpy_coerce_validator(value: Any) -> Any:
+        raise NotImplementedError
+
+    return numpy_coerce_validator
+
+
+def list_coerce(value: Any) -> Any:
+    """
+    Coerces to a value to a list if it isn't already.
+    Used as a BeforeValidator.
+    """
+    if not isinstance(value, list):
+        return [value]
+    return value
 
 def construct_algorithm_model(name: str, model: type[BaseModel]) -> type[BaseModel]:
     """
@@ -26,14 +53,32 @@ def construct_algorithm_model(name: str, model: type[BaseModel]) -> type[BaseMod
     # class AlgorithmParamsCombination(BaseModel):
     #   key1: list[int]
     #   key2: list[list[str]]
-    # This function does not worry about getting the cartesian product of this.
+    # However, we want to preserve certain conveniences (singleton values, fake python evaluation),
+    # so we also make use of BeforeValidators to do so, and we pass over their preferences into the JSON schema.
+    # (Note: This function does not worry about getting the cartesian product of this.)
 
-    # Map our fields to a list (assuming we have no nested keys)
-    mapped_list_field: dict[str, type[list[Any]]] = {name: list[field.annotation] for name, field in model.model_fields.items()}
+    # Map our fields to a list (assuming we have no nested keys),
+    # and specify our user convenience validators
+    mapped_list_field: dict[str, Annotated] = {
+        name: (Annotated[
+            list[field.annotation],
+            # This order isn't arbitrary.
+            # https://docs.pydantic.dev/latest/concepts/validators/#ordering-of-validators
+            # This runs second. This coerces any singletons to lists.
+            BeforeValidator(list_coerce),
+            # This runs first. This evaluates numpy utils for integer/float lists
+            BeforeValidator(
+                python_evalish_coerce(field.annotation),
+                # json_schema_input_type (sensibly) overwrites, so we only specify it here.
+                json_schema_input_type=Union[field.annotation, list[field.annotation], str] if is_numpy_friendly(field.annotation) else \
+                                       Union[field.annotation, list[field.annotation]]
+            )
+        ], field) for name, field in model.model_fields.items()
+    }
 
     # Runtime assertion check: mapped_list_field does not contain any `__-prefixed` fields
     for key in mapped_list_field.keys():
-        assert not key.startswith("__"), f"A private key has been passed from {name}'s argument schema." + \
+        assert not key.startswith("__"), f"A private key has been passed from {name}'s argument schema. " + \
             "This should have been caught by the Snakemake CI step."
 
     # Pass this as kwargs to create_model, which usually takes in parameters field_name=type.
