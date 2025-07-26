18a173f9442ef9acc0b325e8417727e73d1c69b6
Tristan F. <pub.tristanf@gmail.com>
2025-07-25 22:26:06 -0700

    docs: grammar

--- spras/config/schema.py
+++ spras/config/schema.py
@@ -80,7 +80,7 @@ DEFAULT_HASH_LENGTH = 7
 def label_validator(name: str):
     """
     A validator takes in a label
-    and ensure that it's alphanumeric (with underscores).
+    and ensures that it's alphanumeric (with underscores).
     """
     label_pattern = r'^\w+$'
     def validate(label: str):
