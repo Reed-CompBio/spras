d7ad4026b3e6812cf89278fb4d4b4a94e82ba30c
Tristan F. <LeoDog896@hotmail.com>
2025-10-14 01:15:23 +0000

    style: fmt

--- spras/config/config.py
+++ spras/config/config.py
@@ -22,7 +22,7 @@ from typing import Any
 import numpy as np
 import yaml
 
-from spras.config.container_schema import ContainerSettings, ProcessedContainerSettings
+from spras.config.container_schema import ProcessedContainerSettings
 from spras.config.schema import RawConfig
 from spras.util import NpHashEncoder, hash_params_sha1_base32
 
@@ -292,7 +292,7 @@ class Config:
         # Set up a few top-level config variables
         self.out_dir = raw_config.reconstruction_settings.locations.reconstruction_dir
 
-        if raw_config.enable_profiling and not raw_config.containers.framework in ["singularity", "apptainer"]:
+        if raw_config.enable_profiling and raw_config.containers.framework not in ["singularity", "apptainer"]:
             warnings.warn("enable_profiling is set to true, but the container framework is not singularity/apptainer. This setting will have no effect.")
         self.enable_profiling = raw_config.enable_profiling
 
