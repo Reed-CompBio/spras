f528cc1f62a7f1fbaa037f14a043d2912e961272
Tristan F. <pub.tristanf@gmail.com>
2025-11-06 20:25:28 -0800

    fix(meo): use non-deprecated base image
    OpenJDK pulled versions `8` and `11` from DockerHub sometime ago. These containers no longer build.

--- spras/meo.py
+++ spras/meo.py
@@ -178,7 +178,7 @@ class MEO(PRM):
 
         command = ['java', '-jar', '/meo/EOMain.jar', properties_file]
 
-        container_suffix = "meo"
+        container_suffix = "meo:v2"
         run_container_and_log('Maximum Edge Orientation',
                              container_framework,
                              container_suffix,
