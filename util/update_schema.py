5051960427c0082a4cab50b7ed6ac1ee90b1e635
Tristan F. <pub.tristanf@gmail.com>
2026-04-26 10:20:30 +0000

    chore: update schema

--- util/update_schema.py
+++ util/update_schema.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """
 Updates config/schema.json.
 This should be done whenever a new algorithm is introduced,
@@ -9,5 +10,11 @@ from pathlib import Path
 
 from spras.config.schema import RawConfig
 
-config_schema = RawConfig.model_json_schema()
-Path('config', 'schema.json').write_text(json.dumps(config_schema, indent=2))
+current_path = Path(__file__).parent.resolve()
+
+def main():
+    config_schema = RawConfig.model_json_schema()
+    (current_path / '..' / 'config' / 'schema.json').write_text(json.dumps(config_schema, indent=2))
+
+if __name__ == "__main__":
+    main()
